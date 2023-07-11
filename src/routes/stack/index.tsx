6f419b6695959f0c246fbeeba63ddc811fbe2198
Matheus Alves <matheus.adeoliveira11@gmail.com>
2023-07-11 08:49:14 -0300

    Created the main header

--- src/routes/stack/index.tsx
+++ src/routes/stack/index.tsx
@@ -1,8 +1,13 @@
 import { createStackNavigator } from '@react-navigation/stack';
-import { Image } from 'react-native';
+import { Image, View } from 'react-native';
+import { Entypo } from '@expo/vector-icons';
 
 import Login from '../../screens/Login';
 import ChooseUserType from '../../screens/ChooseUserType';
+import Register from '../../screens/Register';
+import Home from '../../screens/home';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { ParamListBase, RouteProp } from '@react-navigation/native';
 
 const { Navigator, Screen } = createStackNavigator();
 
@@ -29,6 +34,37 @@ export default function () {
 				options={{
 					headerShown: false,
 				}}
+			/>
+			<Screen
+				name="Register"
+				component={Register}
+				options={{
+					headerTitle: '',
+				}}
+			/>
+			<Screen
+				name="HomeVariant"
+				component={Home}
+				options={({ route }) => ({
+					headerTitle: route.params.name,
+					headerTitleAlign: 'center',
+					headerTintColor: 'white',
+					headerStyle: {
+						height: 125,
+						backgroundColor: '#292929',
+					},
+					headerLeftContainerStyle: {
+						marginLeft: 12,
+					},
+					headerRight: () => (
+						<TouchableOpacity className="w-12 h-12 bg-gray-500 mr-6 rounded-full overflow-hidden">
+							<Image
+								source={require('../../assets/qodeless_logo.jpg')}
+								className="w-full h-full"
+							/>
+						</TouchableOpacity>
+					),
+				})}
 			/>
 		</Navigator>
 	);
--- src/screens/ChooseUserType/index.tsx
+++ src/screens/ChooseUserType/index.tsx
@@ -1,12 +1,19 @@
-import { View, ImageBackground } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { View, ImageBackground, Button } from 'react-native';
 
 export default function ChooseUserType() {
+	const navigation = useNavigation();
 	return (
 		<ImageBackground
 			source={require('../../assets/football_field.jpg')}
 			className="flex-1 bg-football-field flex flex-col items-center justify-center gap-4"
 		>
-			<View className="w-2/3 h-40 bg-white"></View>
+			<View className="w-2/3 h-40 bg-white">
+				<Button
+					title="Go to next page"
+					onPress={() => navigation.navigate('Register', { name: 'Teste' })} // Problema com tipagem?
+				/>
+			</View>
 			<View className="w-2/3 h-40 bg-white"></View>
 		</ImageBackground>
 	);
