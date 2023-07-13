7b7d91c8c039b6d4083e830b3b9ce6b044396131
Matheus Alves <matheus.adeoliveira11@gmail.com>
2023-07-13 19:56:00 -0300

    changing iugu api route

--- src/services/iugu.ts
+++ src/services/iugu.ts
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 export async function generateInvoice(invoice: CreateInvoiceRequestBody): Promise<CreateInvoiceResponse> {
-  console.log(invoice)
-  const data = await axios.post('http://192.168.0.10:5055/iugu', invoice) as CreateInvoiceResponse; 
+  // const {data} = await axios.post<CreateInvoiceResponse>('http://192.168.0.10:5055/iugu', invoice); 
+  const {data} = await axios.post<CreateInvoiceResponse>('https://api.iugu.com/v1/invoices?api_token=9FA9FCA3E948266BDAE1F8E90B3574487122F3E4495792C9BD2E40D56F9EFC0F', invoice); 
+  console.log({ID_DA_FATURA: data.id})
   return data
 }
