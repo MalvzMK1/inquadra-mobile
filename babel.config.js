4546ad6f490a168a22e3f534a531e484161480a5
Matheus Alves <matheus.adeoliveira11@gmail.com>
2023-07-13 08:54:04 -0300

    Changed start script to clear apollo cache and configured babel compiler with graphql plugin

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@ module.exports = function (api) {
 	api.cache(true);
 	return {
 		presets: ['babel-preset-expo'],
-		plugins: ['nativewind/babel'],
+		plugins: ['nativewind/babel', 'import-graphql'],
 		env: {
 			production: {
 				plugins: ['react-native-paper/babel'],
--- src/lib/apolloClient.ts
+++ src/lib/apolloClient.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-	uri: 'http://localhost:1337/graphql',
+	uri: 'http://192.168.0.10:1337/graphql',
 	cache: new InMemoryCache(),
 });
