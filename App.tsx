d8595cc5ac5d163517bd5e3e058e5fe2e71b1e75
Matheus Alves <matheus.adeoliveira11@gmail.com>
2023-07-13 08:01:46 -0300

    Initializing apollo client

--- App.tsx
+++ App.tsx
@@ -1,10 +1,18 @@
 import { PaperProvider } from 'react-native-paper';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import Routes from './src/routes';
+
+const client = new ApolloClient({
+	uri: 'http://localhost:1337/graphql',
+	cache: new InMemoryCache(),
+});
 
 export default function App() {
 	return (
-		<PaperProvider>
-			<Routes />
-		</PaperProvider>
+		<ApolloProvider client={client}>
+			<PaperProvider>
+				<Routes />
+			</PaperProvider>
+		</ApolloProvider>
 	);
 }
--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,8 +1,14 @@
 import { View, Image } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import { BottomNavigationBar } from '../../components/BottomNavigationBar';
+import {useEffect} from "react";
 
 export default function Home() {
+	useEffect(() => {
+		async function getReservations() {
+
+		}
+	})
 	return (
 		<View className="flex-1 flex flex-col">
 			<View className="flex-1"></View>
