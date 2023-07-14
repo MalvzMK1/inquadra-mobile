a9d1c65e6518a1cebc67392df7ffec7b2018d15a
Matheus Alves <matheus.adeoliveira11@gmail.com>
2023-07-14 11:04:08 -0300

    Applying composition pattern to infosCourt card

--- src/components/InfosCourt/InfosCourtCard.tsx
+++ src/components/InfosCourt/InfosCourtCard.tsx
@@ -1,44 +1,32 @@
 import {Image, ImageSourcePropType, Text, View} from "react-native";
-import SvgUri from "react-native-svg-uri";
+import {ReactNode} from "react";
 
 interface InfosCourtProps {
 	imageUrl: ImageSourcePropType
-	courtName: string
-	courtType: string
-	distance: string
-	lastScheduling: Date
+	children: ReactNode
 }
 
-function formatHour(date: Date): string {
-	return [
-		date.getHours(),
-		date.getMinutes()
-	].join(':');
-}
-
-export function InfosCourtCard({imageUrl, courtName, courtType, lastScheduling, distance}: InfosCourtProps) {
-	const formatedDate = lastScheduling.toLocaleDateString('pt-BR', {
-		hour12: false
-	})
-	const formatedHour = formatHour(lastScheduling)
+export function InfosCourtCard({imageUrl, children}: InfosCourtProps) {
 	return(
-		<View className='w-full min-h-28 flex flex-row items-center justify-between gap-2 overflow-hidden'>
-			<Image source={imageUrl} className='h-max w-34 rounded-md' />
-			<View className='flex-1 flex-1 flex flex-col justify-between'>
-				<View className='w-full flex-1'>
-					<View className='w-full flex flex-row justify-between items-center'>
-						<Text className='font-bold text-base text-orange-600'>{courtName}</Text>
-						<SvgUri
-							height={18}
-							width={18}
-							source={require('../../assets/filled-heart.svg')}
-						/>
-					</View>
-					<Text className='text-white text-xs'>{courtType}</Text>
-					<Text className='text-white text-xs font-bold'>{distance} de distancia</Text>
-				</View>
-				<Text className='text-white text-xs font-bold mt-2'>Última reserva {formatedDate} as {formatedHour}</Text>
-			</View>
+		<View className='w-full min-h-28 flex flex-row items-center justify-between overflow-hidden'>
+			<Image source={imageUrl} className='h-max w-34 rounded-md mr-2' />
+			{children}
+			{/*<View className='flex-1 flex-1 flex flex-col justify-between'>*/}
+			{/*	<View className='w-full flex-1'>*/}
+			{/*		{children}*/}
+			{/*		/!*<View className='w-full flex flex-row justify-between items-center'>*!/*/}
+			{/*		/!*	<Text className='font-bold text-base text-orange-600'>{courtName}</Text>*!/*/}
+			{/*		/!*	<SvgUri*!/*/}
+			{/*		/!*		height={18}*!/*/}
+			{/*		/!*		width={18}*!/*/}
+			{/*		/!*		source={require('../../assets/filled-heart.svg')}*!/*/}
+			{/*		/!*	/>*!/*/}
+			{/*		/!*</View>*!/*/}
+			{/*		<Text className='text-white text-xs'>{courtType}</Text>*/}
+			{/*		<Text className='text-white text-xs font-bold'>{distance} de distancia</Text>*/}
+			{/*	</View>*/}
+			{/*	<Text className='text-white text-xs font-bold mt-2'>Última reserva {formatedDate} as {formatedHour}</Text>*/}
+			{/*</View>*/}
 		</View>
 	)
 }
