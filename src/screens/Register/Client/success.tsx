5da58c01003062ac2af4a3d95ef24d40da5a0b5f
vini00784 <vo70346@gmail.com>
2023-07-14 10:16:20 -0300

    small change

--- src/screens/Register/Client/success.tsx
+++ src/screens/Register/Client/success.tsx
@@ -11,7 +11,7 @@ export default function RegisterSuccess() {
                     <Text className="text-4xl font-semibold">Cadastro feito com sucesso</Text>
                     <Image source={require('../../../assets/inquadra_unnamed_logo.png')} className="w-20 h-20"></Image>
                 </View>
-                <TouchableOpacity className='h-14 w-full rounded-md bg-orange-500 flex items-center justify-center' onPress={() => navigation.navigate('RegisterPassword')}>
+                <TouchableOpacity className='h-14 w-80 rounded-md bg-orange-500 flex items-center justify-center' onPress={() => navigation.navigate('RegisterPassword')}>
                   <Text className='text-gray-50'>Continuar</Text>
                 </TouchableOpacity>
             </View>
