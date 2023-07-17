74a3946adc071156364d8074ea492416ec36cffd
vini00784 <vo70346@gmail.com>
2023-07-17 17:34:01 -0300

    inserting masks to phone number and cpf

--- src/screens/Register/Client/index.tsx
+++ src/screens/Register/Client/index.tsx
@@ -3,9 +3,16 @@ import React, { useState } from 'react';
 import { View, Text, ScrollView, TextInput } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import { RegisterHeader } from '../../../components/RegisterHeader';
+import MaskInput, { Masks } from 'react-native-mask-input';
 
 export default function Register() {
 	const navigation = useNavigation();
+
+	const [ name, setName ] = useState("")
+	const [ email, setEmail ] = useState("")
+	const [ phoneNumber, setPhoneNumber ] = useState("")
+	const [ cpf, setCpf ] = useState("")
+
 	return (
 		<View className="flex-1 bg-white h-screen">
 
@@ -15,22 +22,44 @@ export default function Register() {
 				<View className='p-6 gap-2 flex flex-col justify-between'>
 					<View>
 						<Text className='text-xl'>Qual é o seu nome?</Text>
-						<TextInput className='p-4 border border-neutral-400 rounded' placeholder='Ex.: João'></TextInput>
+						<TextInput 
+							className='p-4 border border-neutral-400 rounded' 
+							placeholder='Ex.: João'
+							value={name}
+							onChangeText={setName}>
+						</TextInput>
 					</View>
 
 					<View>
 						<Text className='text-xl'>Qual é o seu email?</Text>
-						<TextInput className='p-4 border border-neutral-400 rounded' placeholder='exemplo@mail.com.br'></TextInput>
+						<TextInput 
+							className='p-4 border border-neutral-400 rounded' 
+							placeholder='Ex.: João'
+							value={email}
+							onChangeText={setEmail}>
+						</TextInput>
 					</View>
 
 					<View>
 						<Text className='text-xl'>Qual é o número do seu celular?</Text>
-						<TextInput className='p-4 border border-neutral-400 rounded' placeholder='(00) 00000-0000'></TextInput>
+						<MaskInput 
+							className='p-4 border border-neutral-400 rounded' 
+							placeholder='(00) 00000-0000'
+							value={phoneNumber}
+							onChangeText={setPhoneNumber}
+							mask={Masks.BRL_PHONE}>
+						</MaskInput>
 					</View>
 
 					<View>
 						<Text className='text-xl'>Qual é o número do seu CPF?</Text>
-						<TextInput className='p-4 border border-neutral-400 rounded' placeholder='000-000-000-00'></TextInput>
+						<MaskInput 
+							className='p-4 border border-neutral-400 rounded' 
+							placeholder='000-000-000-00'
+							value={cpf}
+							onChangeText={setCpf}
+							mask={Masks.BRL_CPF}>
+						</MaskInput>
 					</View>
 
 				</View>
