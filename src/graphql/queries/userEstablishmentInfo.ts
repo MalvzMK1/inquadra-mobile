0a6b8d06d263fc8034093b5431cc1169d28a7d6f
Matheus Alves <matheus.adeoliveira11@gmail.com>
2023-07-18 10:19:35 -0300

    Created hook to get an establishment iformation from an user

---
+++ src/graphql/queries/userEstablishmentInfo.ts
@@ -0,0 +1,105 @@
+import {gql} from "@apollo/client";
+
+export interface IUserEstablishmentResponse {
+	usersPermissionsUser: {
+		data: {
+			attributes: {
+				username: User['username']
+				email: User['email']
+				phoneNumber: User['phoneNumber']
+				cpf: User['cpf']
+				establishment: {
+					data: {
+						id: Establishment['id']
+						attributes: Omit<Establishment, 'id'> & {
+							pix_keys: {
+								data: Array<{
+									id: PixKey['id']
+									attributes: Omit<PixKey, 'id'>
+								}>
+							}
+						} & {
+							courts: {
+								data: Array<{
+									id: Court['id']
+									attributes: Omit<Court, 'id' | 'rating'>
+								}>
+							}
+						} & {
+							address: Address
+						} & {
+							amenities: {
+								data: Array<{
+									id: Amenitie['id']
+									attributes: Omit<Amenitie, 'id'>
+								}>
+							}
+						}
+					}
+				}
+			}
+		}
+		__typename: string
+	}
+}
+
+export interface IUserEstablishmentVariables {
+	id: string
+}
+
+export const userEstablishmentQuery = gql`
+    query getInfosEstablishment($id: ID) {
+        usersPermissionsUser(id: $id) {
+            data {
+                attributes {
+                    username
+                    email
+                    phoneNumber
+                    cpf
+                    establishment {
+                        data {
+                            id
+                            attributes {
+                                pix_keys {
+                                    data {
+                                        id
+                                        attributes {
+                                            key
+                                        }
+                                    }
+                                }
+                                corporateName
+                                cnpj
+                                fantasyName
+                                address {
+                                    id
+                                    streetName
+                                    cep
+                                    latitude
+                                    longitude
+                                }
+                                amenities {
+                                    data {
+                                        id
+                                        attributes {
+                                            name
+                                        }
+                                    }
+                                }
+                                courts {
+                                    data {
+                                        id
+                                        attributes {
+                                            name
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+`;
