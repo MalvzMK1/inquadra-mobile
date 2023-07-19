59be00238b96569c41a263cd92f271a32ecb6c53
Lucas Camilo <98404472+LusCamilo@users.noreply.github.com>
2023-07-19 01:32:44 -0300

    update popup text

--- src/components/CourtBalloon/index.tsx
+++ src/components/CourtBalloon/index.tsx
@@ -8,7 +8,7 @@ export default function CourtBallon(props:CourtCardInfos) {
     return (
         <Callout className='w-36 h-28 flex flex-col items-center justify-center'>
             <Image className=' h-9 w-9' source={{ uri: props.image }}/>
-            <View className='self-start'>
+            <View className=''>
                 <Text className='font-black text-[#FF6112] text-[10px]'>{props.name}</Text>
                 <Text className='text-[8px]'>{props.type}</Text>
                 <Text className='font-bold text-[8px]'>{props.distance}Km de Distancia</Text>
