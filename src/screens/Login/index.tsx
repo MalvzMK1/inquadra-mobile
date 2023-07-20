7942142a433d727e4f83faf847198059382756c8
Matheus Alves <matheus.adeoliveira11@gmail.com>
2023-07-20 11:42:56 -0300

    Integrated with user authentication

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,23 +1,37 @@
-import React, {useEffect, useState} from 'react'
-import { View, Text, Image, Button } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import React, {useState} from 'react'
+import { View, Text, Image } from 'react-native';
+import {NavigationProp, useNavigation} from '@react-navigation/native';
 import { ScrollView, TouchableOpacity } from 'react-native-gesture-handler';
 import { TextInput } from 'react-native-paper';
-import useRegisterUser from '../../hooks/useRegisterUser';
-import useUpdateUser from '../../hooks/useUpdateUser';
-import useDeleteUser from '../../hooks/useDeleteUser';
-import useRegisterEstablishment from '../../hooks/useRegisterEstablishment';
-import useUpdateEstablishment from '../../hooks/useUpdateEstablishment';
-import useDeleteEstablishment from '../../hooks/useDeleteEstablishment';
-import useRegisterCourt from '../../hooks/useRegisterCourt';
 import useUpdateCourt from '../../hooks/useUpdateCourt';
+import {Controller, useForm} from "react-hook-form";
+import {z} from "zod";
+import {zodResolver} from "@hookform/resolvers/zod";
+import useLoginUser from "../../hooks/useLoginUser";
+import storage from "../../utils/storage";
+
+interface IFormData {
+	identifier: string
+	password: string
+}
+
+const formSchema = z.object({
+	identifier: z.string()
+		.nonempty('O campo não pode estar vazio'),
+	password: z.string()
+		.nonempty('O campo não pode estar vazio')
+})
 
 export default function Login() {
-	const [userEmail, setUserEmail] = useState<string>("");
-	const [userPassWord, setUserPassword] = useState<string>("")
+	const [authUser, {data, loading, error}] = useLoginUser()
+	const navigation = useNavigation<NavigationProp<RootStackParamList>>();
+	const { control, handleSubmit, formState: {errors} } = useForm<IFormData>({
+		resolver: zodResolver(formSchema)
+	})
+
 	const [showPassword, setShowPassword] = useState<boolean>(false)
 
-	const [teste, { data, loading, error }] = useUpdateCourt()
+	const [teste, { data: updateCourtData, loading: updateCourtLoading, error: updateCourtError }] = useUpdateCourt()
 
 	const handleTeste = async () => {
 		try {
@@ -40,96 +54,119 @@ export default function Login() {
 		}
 	  };
 
-	
-
 	const handleShowPassword = () => {
 		setShowPassword(!showPassword);
 	}
 
-	const navigation = useNavigation();
+	function handleLogin(data: IFormData): void {
+		authUser({
+			variables: {
+				identifier: data.identifier.trim(),
+				password: data.password.trim()
+			}
+		}).then(data => {
+			console.log(data.data?.login.jwt)
+			if (data.data) storage.save({
+				key: 'userJWTToken',
+				data: {
+					token: data.data.login.jwt
+				},
+				expires: 1000 * 3600
+			})
+			storage.load({
+				key: 'userJWTToken',
+			}).then(value => console.log({value}))
+		}).catch(err => console.error(err))
+	}
 
 	return (
-		
-			<ScrollView className='flex-1 h-max w-max bg-white'>
+		<ScrollView className='flex-1 h-max w-max bg-white'>
 			<View className='h-16 W-max'></View>
-			<TouchableOpacity className='h-14 w-81 rounded-md bg-orange-500 flex items-center justify-center' onPress={handleTeste} disabled={loading}>
+			<TouchableOpacity className='h-14 w-81 rounded-md bg-orange-500 flex items-center justify-center' onPress={handleTeste} disabled={updateCourtLoading}>
 				<Text className='text-gray-50'>DEUS ABENÇOE</Text>
 			</TouchableOpacity>
 
-			<View className="flex-1 flex items-center justify-center h-max w-max bg-white px-7 ">
+			<View className="flex-1 flex items-center justify-center px-7 bg-violet-500">
 				<TouchableOpacity onPress={() => navigation.navigate('Home')}>
 					<Text className='text-base text-gray-400 pb-5'>Seja bem vindo</Text>
 				</TouchableOpacity>
 
 				<View className="w-full">
-
-				<View className="h-14">
-					<TextInput className= "h-14 text-base"
-						value={userEmail}
-						onChangeText={setUserEmail}
-						outlineColor='#DCDCDC'
-						mode='outlined'
-						label={<Text style={{ color: '#DCDCDC' }}>Email</Text>}
-						left = {
-						<TextInput.Icon
-							icon={'account-outline'}
-							color="#DCDCDC"
-							style={{marginTop: 15}}
-						/>}
-						theme={{
-							colors: {
-								placeholder: '#DCDCDC',
-								primary: '#DCDCDC',
-								text: '#DCDCDC',
-								background: 'white'
-							}
-						}}
+					<Controller
+						name='identifier'
+						control={control}
+						render={({ field: {onChange} }) => (
+							<TextInput
+								className= "h-14 text-base"
+								onChangeText={onChange}
+								outlineColor='#DCDCDC'
+								mode='outlined'
+								label={<Text style={{ color: '#DCDCDC' }}>Email</Text>}
+								left = {
+								<TextInput.Icon
+									icon={'account-outline'}
+									color="#DCDCDC"
+									style={{marginTop: 15}}
+								/>}
+								theme={{
+									colors: {
+										placeholder: '#DCDCDC',
+										primary: '#DCDCDC',
+										text: '#DCDCDC',
+										background: 'white'
+									}
+							}}
+							/>
+						)}
 					/>
-				</View>
-				<View className='h-14 pt-4'>
-					<TextInput className="h-14 text-base"
-						secureTextEntry = {!showPassword}
-						value={userPassWord}
-						onChangeText={setUserPassword}
-						mode='outlined'
-						outlineColor='#DCDCDC'
-						label={<Text style={{ color: '#DCDCDC' }}>******</Text>}
-						left = {
-							<TextInput.Icon
-								icon={'lock-outline'}
-								color="#DCDCDC"
-								style={{marginTop: 15}}
-							/>}
-						right = {
-							<TextInput.Icon
-								icon={!showPassword ? 'eye-off-outline' : 'eye-outline'}
-								color="#DCDCDC"
-								style={{marginTop: 15}}
-								onPress={handleShowPassword}
-							/>}
-
-
-						theme={{
-							colors: {
-								placeholder: '#DCDCDC',
-								primary: '#DCDCDC',
-								text: '#DCDCDC',
-								background: 'white'
+					{errors.identifier && <Text className='text-red-400 text-sm'>{errors.identifier.message}</Text>}
+					<Controller
+						name='password'
+						control={control}
+						render={({ field: { onChange }}) => (
+							<TextInput
+								className="h-14 text-base"
+								secureTextEntry = {!showPassword}
+								onChangeText={onChange}
+								mode='outlined'
+								outlineColor='#DCDCDC'
+								label={<Text style={{ color: '#DCDCDC' }}>******</Text>}
+								left = {
+								<TextInput.Icon
+									icon={'lock-outline'}
+									color="#DCDCDC"
+									style={{marginTop: 15}}
+								/>}
+								right = {
+								<TextInput.Icon
+									icon={!showPassword ? 'eye-off-outline' : 'eye-outline'}
+									color="#DCDCDC"
+									style={{marginTop: 15}}
+									onPress={handleShowPassword}
+								/>
 							}
-						}}
-
+								theme={{
+									colors: {
+										placeholder: '#DCDCDC',
+										primary: '#DCDCDC',
+										text: '#DCDCDC',
+										background: 'white'
+									}
+							}}
+							/>
+						)}
 					/>
-				</View>
-
-
+					{errors.password && <Text className='text-red-400 text-sm'>{errors.password.message}</Text>}
 				<View className='flex items-end pt-8'>
 					<TouchableOpacity>
 						<Text className='text-gray-400 text-base'>esqueceu a senha?</Text>
 					</TouchableOpacity>
 				</View>
-
 				<View className='h-11 pt-4'>
-					<TouchableOpacity className='h-14 w-81 rounded-md bg-orange-500 flex items-center justify-center'>
+
+					<TouchableOpacity
+						className='h-14 w-81 rounded-md bg-orange-500 flex items-center justify-center'
+						onPress={handleSubmit(handleLogin)}>
 						<Text className='text-gray-50'>Entrar</Text>
 					</TouchableOpacity>
 				</View>
@@ -139,7 +176,6 @@ export default function Login() {
 						<Image source={require('../../assets/google.png')} className=''></Image>
 					</TouchableOpacity>
 				</View>
-
 				<View className='flex-row  items-center justify-center pt-11'>
 					<Text className='text-base text-gray-400'>Ainda não tem uma conta?</Text>
 					<TouchableOpacity onPress={() => navigation.navigate('Register')}>
