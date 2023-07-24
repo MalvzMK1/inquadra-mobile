9c032b1aee323ce05944bfdd547a12a7e8c95065
EnzoDPrado <enzodp1501@gmail.com>
2023-07-24 09:14:12 -0300

    tryng to resolve undefined error

--- src/graphql/queries/favoriteById.ts
+++ src/graphql/queries/favoriteById.ts
@@ -8,63 +8,11 @@ import { CourtCardInfos } from "../../types/Court";
 export interface IFavoriteByIdResponse {
     usersPermissionsUser: {
         data: {
-            attributes: {
-                favorite_courts: {
-                    data: Array<{
-                        attributes: {
-                            court_type: {
-                                data: {
-                                    attributes: {
-                                        name: CourtCardInfos['type']
-                                        courts: {
-                                            data: Array<{
-                                                id: Court['id']
-                                                attributes: {
-                                                    court_availabilities: {
-                                                      data : Array<{
-                                                        id: number
-                                                        attributes: {
-                                                          schedulings: {
-                                                            data: Array<{
-                                                              id: number
-                                                              attributes: {
-                                                                date: Date
-                                                              }
-                                                            }>
-                                                          }
-                                                        }
-                                                      }>
-                                                    }
-                                                    name: Court['name']
-                                                    fantasy_name: Court['fantasy_name']
-                                                    photo: {
-                                                        data: Array<{
-                                                            attributes: {
-                                                                url: Photo['url']
-                                                            }
-                                                        }>
-                                                    }
-                                                    establishment: {
-                                                        data: {
-                                                            id: Establishment['id']
-                                                            attributes: {
-                                                                address: {
-                                                                    latitude: Address['latitude']
-                                                                    longitude: Address['longitude']
-                                                                }
-                                                            }
-                                                        }
-                                                    }
-                                                }
-                                            }>
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }>
-                }
+          attributes: {
+            favorite_courts: {
+              data: <>
             }
+          }
         }
     }
 }
