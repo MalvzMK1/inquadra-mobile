a8f02f9503e5dc19d9c584de664e38bf9b232b83
Matheus Alves <matheus.adeoliveira11@gmail.com>
2023-07-31 08:49:46 -0300

    Removing red background from home court card

--- src/components/CourtCardHome/index.tsx
+++ src/components/CourtCardHome/index.tsx
@@ -19,7 +19,7 @@ export default function CourtCardHome(props: CourtCardInfos) {
 			courtImage: props.image,
 			courtName: props.name
 		})}>
-			<View className='flex flex-row flex-1 bg-red-500 gap-x-[14px] mb-5'>
+			<View className='flex flex-row flex-1 gap-x-[14px] mb-5'>
 				{/*<TouchableOpacity className='w-12 h-22 rounded-[10px] bg-red-500' onPress={() => navigation.navigate('CourtAvailibilityInfo', {*/}
 				{/*	courtId: props.id*/}
 				{/*})}>*/}
