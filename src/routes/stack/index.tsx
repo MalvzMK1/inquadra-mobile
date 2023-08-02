5630680f6a98ca79a46f5310e7bb663a83106368
Larissa Nunes <larissanunes@larissa.local>
2023-08-02 06:25:38 -0300

    financial

--- src/routes/stack/index.tsx
+++ src/routes/stack/index.tsx
@@ -25,6 +25,7 @@ import CourtDetails from '../../screens/AllVeryWell/CourtDetails';
 import HomeEstablishment from '../../screens/HomeEstablishment';
 import InfoProfileEstablishment from '../../screens/ProfileEstablishmentRegistration/Client/InfoProfileEstablishment';
 import DeleteAccountEstablishment from '../../screens/ProfileEstablishmentRegistration/Client/deleteAccount';
+import FinancialEstablishment from '../../screens/FinancialEstablishment';
 
 const { Navigator, Screen } = createStackNavigator<RootStackParamList>();
 
@@ -34,6 +35,33 @@ export default function () {
 
 	return (
 		<Navigator>
+			<Screen
+				name="FinancialEstablishment"
+				component={FinancialEstablishment}
+				options={{
+					headerTintColor: 'white',
+					headerStyle: {
+						height: 100,
+						backgroundColor: '#292929',
+					},
+					headerTitleAlign: 'center',
+					headerTitle: () => (
+						<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+							<Text style={{ color: 'white', fontSize: 18, fontWeight: '900' }}>PERFIL</Text>
+						</View>
+					),
+					headerRight: () => (
+						<TouchableOpacity style={{ paddingRight: 10 }}>
+							<Image source={require('../../assets/picture.png')} style={{ width: 30, height: 30, borderRadius: 15 }} />
+						</TouchableOpacity>
+					),
+					headerLeft: ({ navigation }) => (
+						<TouchableOpacity onPress={() => navigation.navigate('Login')}>
+							<Icon name="arrow-back" size={25} color="white" />
+						</TouchableOpacity>
+					),
+				}}
+			/>
 			<Screen
 				name="DeleteAccountEstablishment"
 				component={DeleteAccountEstablishment}
