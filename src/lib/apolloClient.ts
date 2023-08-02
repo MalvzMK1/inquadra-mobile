b942aea68edb1289575a4390ca65fe49bf748b4c
Matheus Alves <matheus.adeoliveira11@gmail.com>
2023-08-02 08:43:14 -0300

    Changing apollo url to .env HOST_API

--- src/lib/apolloClient.ts
+++ src/lib/apolloClient.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
+import {HOST_API} from '@env'
 
 export const client = new ApolloClient({
-	uri: 'http://192.168.15.5:1337' + '/graphql',
-	// uri: process.env.HOST_API + '/graphql',
+	uri: HOST_API + '/graphql',
 	cache: new InMemoryCache(),
 });
