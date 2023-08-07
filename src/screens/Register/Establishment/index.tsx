8d0854e251ce1edc222b78f494098b25d4c0ad6e
Matheus Alves <matheus.adeoliveira11@gmail.com>
2023-08-07 08:30:39 -0300

    Integrating the amenities in the amenities select list

--- src/screens/Register/Establishment/index.tsx
+++ src/screens/Register/Establishment/index.tsx
@@ -1,6 +1,6 @@
-import { View, Text, TouchableOpacity, TextInput, Image, FlatList } from "react-native";
+import {View, Text, TouchableOpacity, TextInput, Image, FlatList, ActivityIndicator} from "react-native";
 
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
 import MaskInput, { Masks } from 'react-native-mask-input';
 import { ScrollView } from "react-native-gesture-handler";
 import * as ImagePicker from 'expo-image-picker';
@@ -8,8 +8,9 @@ import { AntDesign, Ionicons } from "@expo/vector-icons";
 import { MultipleSelectList } from 'react-native-dropdown-select-list';
 import {Controller, useForm} from "react-hook-form";
 import {zodResolver} from "@hookform/resolvers/zod";
-import {string, z} from 'zod'
+import {z} from 'zod'
 import {NativeStackScreenProps} from "@react-navigation/native-stack";
+import useAllAmenities from "../../../hooks/useAllAmenities";
 
 interface IFormSchema {
 	name: string,
@@ -54,20 +55,19 @@ export default function RegisterEstablishment({navigation, route}: NativeStackSc
 	})
 
 	const [profilePicture, setProfilePicture] = useState(null);
-
 	const [photos, setPhotos] = useState<Array<{uri: string}>>([]);
-
 	const [selected, setSelected] = useState<Array<string>>([]);
+	const [selectAmenitiesData, setSelectAmenitiesData] = useState<Array<{key: string, value: string}>>([])
 
-	const data = [
-		{id:'1', value:'Estacionamento'},
-		{id:'2', value:'Vestiário'},
-		{id:'3', value:'Restaurante'},
-		{id:'4', value:'Opção 4'},
-		{id:'5', value:'Opção 5'},
-		{id:'6', value:'Opção 6'},
-		{id:'7', value:'Opção 7'},
-	]
+	function handleAmenitieClick(providedAmenitieId: Array<string>): void {
+		console.log();
+	}
+
+	const {
+		data: amenitiesData,
+		loading: amenitiesLoading,
+		error: amenitiesError
+	} = useAllAmenities();
 
 	const [selectedItems, setSelectedItems] = useState([]);
 
@@ -117,175 +117,207 @@ export default function RegisterEstablishment({navigation, route}: NativeStackSc
 		})
 	}
 
+	useEffect(() => {
+		if (!amenitiesLoading && !amenitiesError && amenitiesData?.amenities.data) {
+			const updatedAmenitiesData = amenitiesData?.amenities.data.map(amenitie => ({
+				key: amenitie.id,
+				value: amenitie.attributes.name,
+			}));
+
+			console.log(selectAmenitiesData, updatedAmenitiesData)
+
+			selectAmenitiesData.forEach(amenitie => {
+				updatedAmenitiesData.forEach((newAmenitie, index) => {
+					if (amenitie.key === newAmenitie.key) updatedAmenitiesData.splice(index, index)
+				})
+			})
+
+			setSelectAmenitiesData(prevState => [...prevState, ...updatedAmenitiesData]);
+		}
+	}, [amenitiesData])
+
 	return (
 		<ScrollView className="h-fit bg-white flex-1">
 			{errors && <Text>{JSON.stringify(errors)}</Text>}
-			<View className="items-center mt-9 p-4">
-				<Text className="text-3xl text-center font-extrabold text-gray-700">Cadastro{'\n'}Estabelecimento</Text>
-			</View>
-			<View className='h-fit'>
-				<View className='p-5 gap-7 flex flex-col justify-between'>
-					<View>
-						<Text className='text-xl p-1'>Nome do Estabelecimento</Text>
-						<Controller 
-							name='name'
-							control={control}
-							render={({field: {onChange}}) => (
-								<TextInput 
-									className='p-5 border border-neutral-400 rounded' 
-									placeholder='Ex.: Quadra do Zeca' 
-									onChangeText={onChange}
-								/>
-							)}
-						/>
-						{errors.name && <Text className='text-red-400 text-sm'>{errors.name.message}</Text> }
+			{amenitiesLoading && selectAmenitiesData.length === 0 ? <ActivityIndicator size='large' color='#F5620F' /> :
+				<View>
+					<View className="items-center mt-9 p-4">
+					<Text className="text-3xl text-center font-extrabold text-gray-700">Cadastro{'\n'}Estabelecimento</Text>
+				</View>
+						<View className='h-fit'>
+							<View className='p-5 gap-7 flex flex-col justify-between'>
+								<View>
+									<Text className='text-xl p-1'>Nome do Estabelecimento</Text>
+									<Controller
+										name='name'
+										control={control}
+										render={({field: {onChange}}) => (
+											<TextInput
+												className='p-5 border border-neutral-400 rounded'
+												placeholder='Ex.: Quadra do Zeca'
+												onChangeText={onChange}
+											/>
+										)}
+									/>
+					{errors.name && <Text className='text-red-400 text-sm'>{errors.name.message}</Text> }
 					</View>
-					<View>
-						<Text className="text-xl p-1">CNPJ</Text>
-						<Controller
-							name='cnpj'
-							control={control}
-							render={({field: {onChange}}) => (
-								<MaskInput
-									className='p-5 border border-neutral-400 rounded'
-									placeholder='00.000.000/0001-00.'
-									value={getValues('cnpj')}
-									maxLength={18}
-									keyboardType={'numeric'}
-									onChangeText={(masked, unmasked) => onChange(unmasked)}
-									mask={Masks.BRL_CNPJ}
-								/>
-							)}
-						/>
-						{errors.cnpj && <Text className='text-red-400 text-sm'>{errors.cnpj.message}</Text>}
-					</View>
-					<View>
-						<Text className="text-xl p-1">Telefone para Contato</Text>
-						<Controller
-							name='phone'
-							control={control}
-							render={({field: {onChange}}) => (
-								<MaskInput
-									className='p-5 border border-neutral-400 rounded'
-									placeholder='(00) 0000-0000'
-									value={getValues('phone')}
-									maxLength={15}
-									keyboardType={'numeric'}
-									onChangeText={(masked, unmasked) => onChange(unmasked)}
-									mask={Masks.BRL_PHONE}
-								/>
-							)}
-						/>
-						{errors.phone && <Text className='text-red-400 text-sm'>{errors.phone.message}</Text>}
-					</View>
-					<View>
-						<Text className='text-xl p-1'>Endereço</Text>
-						<Controller
-							name='address.streetName'
-							control={control}
-							render={({field: {onChange}}) => (
-								<TextInput
-									className='p-5 border border-neutral-400 rounded'
-									placeholder='Rua Rufus'
-									onChangeText={onChange}
-								/>
-							)}
-						/>
-						{errors.address?.streetName && <Text className='text-red-400 text-sm'>{errors.address?.streetName.message}</Text>}
-					</View>
-					<View className="flex flex-row justify-between">
 						<View>
-							<Text className='text-xl p-1'>Número</Text>
+							<Text className="text-xl p-1">CNPJ</Text>
 							<Controller
-								name='address.number'
+								name='cnpj'
+								control={control}
+								render={({field: {onChange}}) => (
+									<MaskInput
+										className='p-5 border border-neutral-400 rounded'
+										placeholder='00.000.000/0001-00.'
+										value={getValues('cnpj')}
+										maxLength={18}
+										keyboardType={'numeric'}
+										onChangeText={(masked, unmasked) => onChange(unmasked)}
+										mask={Masks.BRL_CNPJ}
+									/>
+								)}
+							/>
+							{errors.cnpj && <Text className='text-red-400 text-sm'>{errors.cnpj.message}</Text>}
+						</View>
+						<View>
+							<Text className="text-xl p-1">Telefone para Contato</Text>
+							<Controller
+								name='phone'
+								control={control}
+								render={({field: {onChange}}) => (
+									<MaskInput
+										className='p-5 border border-neutral-400 rounded'
+										placeholder='(00) 0000-0000'
+										value={getValues('phone')}
+										maxLength={15}
+										keyboardType={'numeric'}
+										onChangeText={(masked, unmasked) => onChange(unmasked)}
+										mask={Masks.BRL_PHONE}
+									/>
+								)}
+							/>
+							{errors.phone && <Text className='text-red-400 text-sm'>{errors.phone.message}</Text>}
+						</View>
+						<View>
+							<Text className='text-xl p-1'>Endereço</Text>
+							<Controller
+								name='address.streetName'
 								control={control}
 								render={({field: {onChange}}) => (
 									<TextInput
 										className='p-5 border border-neutral-400 rounded'
-										placeholder='123'
+										placeholder='Rua Rufus'
 										onChangeText={onChange}
-										keyboardType={'numbers-and-punctuation'}
 									/>
 								)}
 							/>
-							{errors.address?.number && <Text className='text-red-400 text-sm'>{errors.address?.number.message}</Text>}
+							{errors.address?.streetName && <Text className='text-red-400 text-sm'>{errors.address?.streetName.message}</Text>}
 						</View>
+						<View className="flex flex-row justify-between">
+							<View>
+								<Text className='text-xl p-1'>Número</Text>
+								<Controller
+									name='address.number'
+									control={control}
+									render={({field: {onChange}}) => (
+										<TextInput
+											className='p-5 border border-neutral-400 rounded'
+											placeholder='123'
+											onChangeText={onChange}
+											keyboardType={'numbers-and-punctuation'}
+										/>
+									)}
+								/>
+								{errors.address?.number && <Text className='text-red-400 text-sm'>{errors.address?.number.message}</Text>}
+							</View>
+							<View>
+								<Text className='text-xl p-1'>CEP</Text>
+								<Controller
+									name='address.cep'
+									control={control}
+									render={({field: {onChange}}) => (
+										<MaskInput
+											className='p-5 border border-neutral-400 rounded w-44'
+											placeholder='0000-000'
+											value={getValues('address.cep')}
+											onChangeText={onChange}
+											maxLength={9}
+											keyboardType={'numeric'}
+											mask={Masks.ZIP_CODE}
+										/>
+									)}
+								/>
+								{errors.address?.cep && <Text className='text-red-400 text-sm'>{errors.address?.cep.message}</Text>}
+							</View>
+						</View>
+					</View>
 						<View>
-							<Text className='text-xl p-1'>CEP</Text>
+							<Text className="text-xl p-1">Amenidades do Local</Text>
 							<Controller
-								name='address.cep'
+								name='amenities'
 								control={control}
 								render={({field: {onChange}}) => (
-									<MaskInput
-										className='p-5 border border-neutral-400 rounded w-44'
-										placeholder='0000-000'
-										value={getValues('address.cep')}
-										onChangeText={onChange}
-										maxLength={9}
-										keyboardType={'numeric'}
-										mask={Masks.ZIP_CODE}
+									<MultipleSelectList
+										setSelected={handleAmenitieClick}
+										// @ts-ignore
+										data={selectAmenitiesData}
+										// data={() => {
+										// 	if (!amenitiesLoading && amenitiesData)
+										// 		return amenitiesData.amenities.data.map(amenitie => ({
+										// 			value: amenitie.attributes.name,
+										// 			key: amenitie.id
+										// 		}));
+										// 	return [{key: '0', value: 'Nenhuma facilidade encontrada'}];
+										// }}
+										save={'key'}
+										placeholder="Selecione aqui..."
+										label="Amenidades escolhidas:"
+										boxStyles={{borderRadius: 4, minHeight: 55}}
+										inputStyles={{color: "#FF6112", alignSelf: "center"}}
+										searchPlaceholder="Procurar"
+										badgeStyles={{ backgroundColor: "#FF6112"}}
+										closeicon={<Ionicons name="close" size={20} color="#FF6112" />}
+										searchicon={<Ionicons name="search" size={18} color="#FF6112" style={{ marginEnd: 10 }} />}
+										arrowicon={<AntDesign name="down" size={13} color="#FF6112" style={{marginEnd: 2, alignSelf: "center"}} />}
 									/>
 								)}
 							/>
-							{errors.address?.cep && <Text className='text-red-400 text-sm'>{errors.address?.cep.message}</Text>}
+							{errors.amenities && <Text className='text-red-400 text-sm'>{errors.amenities.message}</Text>}
 						</View>
-					</View>
-				</View>
-				<View>
-					<Text className="text-xl p-1">Amenidades do Local</Text>
-					<Controller
-						name='amenities'
-						control={control}
-						render={({field: {onChange}}) => (
-							<MultipleSelectList
-								setSelected={(val: Array<string>) => setSelected(val)}
-								data={data}
-								save={'key'}
-								placeholder="Selecione aqui..."
-								label="Amenidades escolhidas:"
-								boxStyles={{borderRadius: 4, minHeight: 55}}
-								inputStyles={{color: "#FF6112", alignSelf: "center"}}
-								searchPlaceholder="Procurar"
-								badgeStyles={{ backgroundColor: "#FF6112"}}
-								closeicon={<Ionicons name="close" size={20} color="#FF6112" />}
-								searchicon={<Ionicons name="search" size={18} color="#FF6112" style={{ marginEnd: 10 }} />}
-								arrowicon={<AntDesign name="down" size={13} color="#FF6112" style={{marginEnd: 2, alignSelf: "center"}} />}
-							/>
-						)}
-					/>
-					{errors.amenities && <Text className='text-red-400 text-sm'>{errors.amenities.message}</Text>}
-				</View>
-				<View>
-					<Text className="text-xl p-1">Fotos do estabelecimento</Text>
+						<View>
+							<Text className="text-xl p-1">Fotos do estabelecimento</Text>
 
-					<View className="border rounded relative">
-						<View className="flex flex-row">
-							<Text className="text-base text-gray-400 font-bold m-6" onPress={handleProfilePictureUpload}>Carregue as fotos do Estabelecimento. {"\n"} Ex: frente, o bar, o vestiário e afins. </Text>
-							<Ionicons name="star-outline" size={20} color="#FF6112" style = {{ marginTop: 35, marginLeft: 15 }} onPress={handleProfilePictureUpload} />
-						</View>
-
-						<FlatList
-							className="h-max"
-							data={photos}
-							renderItem={({ item, index }) => (
-								<View style={{ flexDirection: 'row', alignItems: 'center' }}>
-									<Image source={{ uri: item.uri }} style={{ width: 100, height: 100, margin: 10}} />
-									<TouchableOpacity style={{ position: 'absolute', right: 0, left: 0, bottom: 0, top: 0, justifyContent: 'center', alignItems: 'center' }} onPress={() => handleDeletePhoto(index)}>
-										<Ionicons name="trash" size={20} color="orange" />
-									</TouchableOpacity>
+							<View className="border rounded relative">
+								<View className="flex flex-row">
+									<Text className="text-base text-gray-400 font-bold m-6" onPress={handleProfilePictureUpload}>Carregue as fotos do Estabelecimento. {"\n"} Ex: frente, o bar, o vestiário e afins. </Text>
+									<Ionicons name="star-outline" size={20} color="#FF6112" style = {{ marginTop: 35, marginLeft: 15 }} onPress={handleProfilePictureUpload} />
 								</View>
-							)}
-							keyExtractor={(item, index) => index.toString()}
-							horizontal
-						/>
+
+								<FlatList
+									className="h-max"
+									data={photos}
+									renderItem={({ item, index }) => (
+										<View style={{ flexDirection: 'row', alignItems: 'center' }}>
+											<Image source={{ uri: item.uri }} style={{ width: 100, height: 100, margin: 10}} />
+											<TouchableOpacity style={{ position: 'absolute', right: 0, left: 0, bottom: 0, top: 0, justifyContent: 'center', alignItems: 'center' }} onPress={() => handleDeletePhoto(index)}>
+												<Ionicons name="trash" size={20} color="orange" />
+											</TouchableOpacity>
+										</View>
+									)}
+									keyExtractor={(item, index) => index.toString()}
+									horizontal
+								/>
+							</View>
+						</View>
+						<View>
+							<TouchableOpacity className='h-14 w-81 rounded-md bg-[#FF6112] items-center justify-center' onPressIn={handleSubmit(submitForm)}>
+								<Text className='text-gray-50'>Continuar</Text>
+							</TouchableOpacity>
+						</View>
 					</View>
 				</View>
-				<View>
-					<TouchableOpacity className='h-14 w-81 rounded-md bg-[#FF6112] items-center justify-center' onPressIn={handleSubmit(submitForm)}>
-						<Text className='text-gray-50'>Continuar</Text>
-					</TouchableOpacity>
-				</View>
-			</View>
+			}
 		</ScrollView>
 	);
 }
