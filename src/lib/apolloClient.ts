d304c6d19a69ed2e51bb33bf82c55ddc0d052cdd
Matheus Alves <matheus.adeoliveira11@gmail.com>
2023-08-09 10:14:45 -0300

    Fixing apollo client initialization

--- src/lib/apolloClient.ts
+++ src/lib/apolloClient.ts
@@ -1,5 +1,5 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
-import { HOST_API } from '@env'
+import { HOST_API } from '@env';
 
 export const client = new ApolloClient({
 	uri: HOST_API + "/graphql",
