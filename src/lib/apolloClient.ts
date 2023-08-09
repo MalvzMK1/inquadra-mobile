d0f14c1f3719c3506f2862edd070d7f1b84b49ee
Matheus Alves <matheus.adeoliveira11@gmail.com>
2023-08-09 08:25:09 -0300

    Fixing apollo client

--- src/lib/apolloClient.ts
+++ src/lib/apolloClient.ts
@@ -2,7 +2,6 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 import { HOST_API } from '@env'
 
 export const client = new ApolloClient({
-	// uri: "http://192.168.15.5:1337/" + "graphql",
-	uri: HOST_API + "graphql",
+	uri: HOST_API + "/graphql",
 	cache: new InMemoryCache(),
 });
