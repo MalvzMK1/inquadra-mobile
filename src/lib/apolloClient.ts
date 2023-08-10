563f095e8d84fee27246138c9563aaa2b328f31e
Matheus Alves <matheus.adeoliveira11@gmail.com>
2023-08-10 10:01:11 -0300

    Initializing Apollo Client with authorization header

--- src/lib/apolloClient.ts
+++ src/lib/apolloClient.ts
@@ -1,7 +1,21 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import {ApolloClient, ApolloLink, HttpLink, InMemoryCache} from "@apollo/client";
 import { HOST_API } from '@env';
 
+const link = new HttpLink({
+	uri: HOST_API + '/graphql',
+})
+
+const authLink = new ApolloLink((operation, forward) => {
+	const token = '' // TODO: pegar o token -> STORAGE
+	operation.setContext({
+		headers: {
+			Authorization: 'bearer ' + token
+		}
+	})
+	return forward(operation)
+})
+
 export const client = new ApolloClient({
-	uri: HOST_API + "/graphql",
+	link: authLink.concat(link),
 	cache: new InMemoryCache(),
 });
