c5a3438e0c2c2fc02fa0e1f42e890fa7cea7cdac
vini00784 <vo70346@gmail.com>
2023-08-15 11:53:38 -0300

    small corrections

--- src/components/CourtCardHome/index.tsx
+++ src/components/CourtCardHome/index.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { View, Image, Text } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
 import { TouchableOpacity } from 'react-native-gesture-handler';
-import {NavigationProp, useNavigation} from '@react-navigation/native';
+import { NavigationProp, useNavigation } from '@react-navigation/native';
 
 export default function CourtCardHome(props: CourtCardInfos) {
 	const navigation = useNavigation<NavigationProp<RootStackParamList>>()
@@ -12,7 +12,7 @@ export default function CourtCardHome(props: CourtCardInfos) {
 	return (
 		<TouchableOpacity onPress={() => navigation.navigate('EstablishmentInfo', {
 			courtID: props.id,
-			userPhoto:  undefined,
+			userPhoto: undefined,
 		})}>
 			<View className='flex flex-row flex-1 gap-x-[14px] mb-5'>
 				{/*<TouchableOpacity className='w-12 h-22 rounded-[10px] bg-red-500' onPress={() => navigation.navigate('CourtAvailibilityInfo', {*/}
@@ -21,13 +21,19 @@ export default function CourtCardHome(props: CourtCardInfos) {
 				{/*	<Image className='w-inherit h-inherit rounded-[10px]' source={{ uri: props.image }} />*/}
 				<Image className='w-[40%] h-[85px] rounded-[10px]' source={{ uri: props.image }} />
 				{/*</TouchableOpacity>*/}
-				<View className='flex flex-row flex-1'>
+				<View className='flex flex-row flex-1 '>
 					<View className='flex-1'>
 						<Text className='text-[#ff6112] font-black text-[15px]'>{props.name}</Text>
 						<Text className='text-white font-bold text-xs'>{props.type}</Text>
-						<Text className='text-white font-bold text-xs'>{props.distance}Km de distacia</Text>
+						<Text className='text-white font-bold text-xs'>{props.distance} Km de distância</Text>
 					</View>
-					<AntDesign name="heart" size={20} color={color} onPress={() => color == "white" ? setColor("red") : setColor("white")} />
+					<AntDesign name="heart" size={20} color={color}
+						onPress={
+							() => {
+								color == "white" ? setColor("red") : setColor("white")
+							}
+						}
+					/>
 				</View>
 			</View>
 		</TouchableOpacity>
