17643712f3794f02269d45f0b36b34bca4e8faa8
vini00784 <vo70346@gmail.com>
2023-08-15 11:52:53 -0300

    taking courts from API

--- src/components/BarHome/index.tsx
+++ src/components/BarHome/index.tsx
@@ -10,21 +10,26 @@ import Animated, {
 } from 'react-native-reanimated';
 import CourtCardHome from '../CourtCardHome';
 
+import { useGetNextToCourts } from '../../hooks/useNextToCourts';
+import { calculateDistance } from '../../utils/calculateDistance';
+import storage from '../../utils/storage';
+
+let userLatitude: number
+let userLongitude: number
+
+storage.load<{ latitude: number, longitude: number }>({
+	key: 'userGeolocation'
+}).then(data => {
+	userLatitude = data.latitude
+	userLongitude = data.longitude
+})
+
 interface HomeBarProps {
-	courts: Array<{
-		id: string,
-		latitude: number,
-		longitude: number,
-		name: string,
-		type: string,
-		image: string,
-		distance: number,
-	}>,
 	userName: string | undefined
 	// photoUser: string | undefined
 }
 
-export default function HomeBar({courts, userName, photoUser}: HomeBarProps) {
+export default function HomeBar({ userName, photoUser }: HomeBarProps) {
 	const [expanded, setExpanded] = useState(false);
 	const height = useSharedValue('40%');
 
@@ -42,6 +47,8 @@ export default function HomeBar({courts, userName, photoUser}: HomeBarProps) {
 		};
 	});
 
+	const { data, error, loading } = useGetNextToCourts()
+
 	return (
 		<Animated.View entering={FadeIn.duration(500)} exiting={FadeOut.duration(500)} style={[animatedStyle, { backgroundColor: "#292929", borderTopEndRadius: 20, borderTopStartRadius: 20 }]}>
 			<View
@@ -52,15 +59,15 @@ export default function HomeBar({courts, userName, photoUser}: HomeBarProps) {
 				<Text className='text-white text-lg font-black mt-3'>Olá{userName ? `, ${userName}` : null}!</Text>
 			</View>
 			<ScrollView className='p-5'>
-				{courts !== undefined ? courts.map((item) => (
+				{data?.courts.data !== undefined ? data?.courts.data.map((item) => (
 					<CourtCardHome
 						key={item.id}
 						id={item.id}
 						// photoUser={photoUser}
-						image={item.image}
-						name={item.name}
-						distance={item.distance}
-						type={item.type}
+						image={item.attributes.photo.data[0].attributes.url}
+						name={item.attributes.name}
+						distance={calculateDistance(userLatitude, userLongitude, parseFloat(item.attributes.establishment.data.attributes.address.latitude), parseFloat(item.attributes.establishment.data.attributes.address.longitude)).toFixed(0)}
+						type={item.attributes.court_type.data.attributes.name}
 					/>
 				)) : <ActivityIndicator size='small' color='#fff' />}
 			</ScrollView>
