5ae0f3e3a3062921228b22adb7ff90b7d623009b
vini00784 <vo70346@gmail.com>
2023-08-15 11:39:12 -0300

    adapting the apolloClient connection

--- src/lib/apolloClient.ts
+++ src/lib/apolloClient.ts
@@ -1,8 +1,30 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-import { HOST_API } from '@env'
+import { ApolloClient, ApolloLink, HttpLink, InMemoryCache } from "@apollo/client";
+// import { HOST_API } from '@env'
+import storage from "../utils/storage";
+
+const link = new HttpLink({
+	uri: "http://192.168.15.19:1337/" + "graphql"
+})
+
+let jwt: string = ""
+
+storage.load<UserInfos>({
+	key: 'userInfos',
+}).then((data) => {
+	jwt = data.token
+})
+
+const authLink = new ApolloLink((operation, forward) => {
+	const token = jwt // TODO: pegar o token -> STORAGE
+	operation.setContext({
+		headers: {
+			Authorization: 'bearer ' + token
+		}
+	})
+	return forward(operation)
+})
 
 export const client = new ApolloClient({
-	// uri: "http://192.168.15.5:1337/" + "graphql",
-	uri: HOST_API + "graphql",
+	link: authLink.concat(link),
 	cache: new InMemoryCache(),
 });
