97ae796c9e9daa091bf021d6fe6b5e592cd5bad7
vini00784 <vo70346@gmail.com>
2023-08-24 10:46:02 -0300

    new screen added

--- src/routes/stack/index.tsx
+++ src/routes/stack/index.tsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { Image, View } from 'react-native';
 import { TextInput, Text } from 'react-native-paper';
@@ -44,6 +44,7 @@ import RegisterNewCourtAdded from '../../screens/RegisterCourt/newCourtAdded';
 import editCourt from '../../screens/AllVeryWell/CourtDetails/editCourt';
 import CourtAvailabilityInfo from "../../screens/CourtAvailabilityInfo";
 import storage from "../../utils/storage";
+import CourtSchedule from '../../screens/CourtSchedule';
 
 const { Navigator, Screen } = createStackNavigator<RootStackParamList>();
 
@@ -209,7 +210,33 @@ export default function () {
 					),
 				}}
 			/>
-
+			<Screen
+				name="CourtSchedule"
+				component={CourtSchedule}
+				options={({ route: { params } }) => ({
+					headerTintColor: 'white',
+					headerStyle: {
+						height: 85,
+						backgroundColor: '#292929',
+					},
+					headerTitleAlign: 'center',
+					headerTitle: () => (
+						<View className='flex-1 justify-center items-center'>
+							<Text className='text-white text-[18px] font-black'>Agenda</Text>
+						</View>
+					),
+					headerRight: () => (
+						<TouchableOpacity className='pr-[10px]'>
+							<Image source={params.establishmentPhoto ? { uri: `${HOST_API}${params.establishmentPhoto}` } : require('../../assets/default-user-image.png')} className='w-[30px] h-[30px] rounded-[15px]' />
+						</TouchableOpacity>
+					),
+					headerLeft: ({ navigation }) => (
+						<TouchableOpacity onPress={() => navigation.goBack()}>
+							<Icon name="arrow-back" size={25} color="white" />
+						</TouchableOpacity>
+					)
+				})}
+			/>
 			<Screen
 				name='RegisterEstablishmentProfile'
 				component={registerEstablishmentProfile}
