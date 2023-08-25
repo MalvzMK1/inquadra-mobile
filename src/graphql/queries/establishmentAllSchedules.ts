3137ebe915644f6d9073a88ff7a5a1ad4ac5e8e4
vini00784 <vo70346@gmail.com>
2023-08-25 11:00:10 -0300

    small changes in hook response

--- src/graphql/queries/establishmentAllSchedules.ts
+++ src/graphql/queries/establishmentAllSchedules.ts
@@ -7,8 +7,9 @@ export interface IEstablishmentAllSchedulesResponse {
 			attributes: {
 				courts: {
 					data: Array<{
-						id: Court['id'],
+						id: Court['id']
 						attributes: {
+							name: Court['name']
 							photo: {
 								data: Array<{
 									attributes: {
@@ -29,6 +30,7 @@ export interface IEstablishmentAllSchedulesResponse {
 										startsAt: string,
 										endsAt: string,
 										status: boolean,
+										weekDay: string,
 										schedulings: {
 											data: Array<{
 												id: string
@@ -62,6 +64,7 @@ export const allEstablishmentSchedulesQuery = gql`
                         data {
                             id
                             attributes {
+								name
                                 photo {
                                     data {
                                         attributes {
@@ -81,7 +84,8 @@ export const allEstablishmentSchedulesQuery = gql`
                                         attributes {
                                             startsAt
                                             endsAt
-		                                        status
+		                                    status
+											weekDay
                                             schedulings {
                                                 data {
 		                                                id
