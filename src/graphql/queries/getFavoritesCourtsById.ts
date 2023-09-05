301ba26cf90232a2cee80379dad82dd0b32fcfc8
EnzoDPrado <enzodp1501@gmail.com>
2023-09-05 10:43:12 -0300

    screen favorite in 99% just need to see when the user did the last schedule

---
+++ src/graphql/queries/getFavoritesCourtsById.ts
@@ -0,0 +1,102 @@
+import { gql } from "@apollo/client";
+import { CourtType } from "../../__generated__/graphql";
+
+export interface IFavoritesCourtsByIdResponse {
+    courtTypes: {
+        data: Array<{
+            attributes:{
+                name: CourtType['name']
+                courts: {
+                    data: Array<{
+                        id: Court['id']
+                        attributes: {
+                            fantasy_name: Court['fantasy_name']
+                            name: Court['name']
+                            photo: {
+                                data: Array<{
+                                    attributes: {
+                                        url: Photo['url']
+                                    }
+                                }>
+                            }
+                            establishment: {
+                                data: {
+                                    attributes: {
+                                        address: {
+                                            latitude: Address['latitude']
+                                            longitude: Address['longitude']
+                                        }
+                                    }
+                                }
+                            }
+                            court_availabilities: {
+                                data: Array<{
+                                    attributes: {
+                                        schedulings: {
+                                            data: Array<{
+                                                attributes: {
+                                                    date: Scheduling['date']
+                                                }
+                                            }>
+                                        }
+                                    }
+                                }>
+                            }
+                        }
+                    }>
+                }
+            }
+        }>
+    }
+}
+
+export const favoriteCourtByIdQuery = gql`
+query getFavoriteById($id: ID) {
+  courtTypes(filters: { courts: { favorited_user: { id: { eq: $id } } } }) {
+    data {
+      attributes {
+        name
+        courts(filters: { favorited_user: { id: { eq: $id } } }) {
+          data {
+            id
+            attributes {
+              fantasy_name
+              name
+              photo {
+                data {
+                  attributes {
+                    url
+                  }
+                }
+              }
+              establishment {
+                data {
+                  attributes {
+                    address {
+                      latitude
+                      longitude
+                    }
+                  }
+                }
+              }
+              court_availabilities {
+                data {
+                  attributes {
+                    schedulings(filters: { owner: { id: { eq: $id } } }) {
+                      data {
+                        attributes {
+                          date
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+`
--- src/screens/FavoriteCourts/index.tsx
+++ src/screens/FavoriteCourts/index.tsx
@@ -1,49 +1,146 @@
-import {ScrollView} from "react-native";
-import {InfosCourt} from "../../components/InfosCourt";
+import { ScrollView } from "react-native";
+import { InfosCourt } from "../../components/InfosCourt";
 import { useGetFavoriteById } from "../../hooks/useFavoriteById"
 import { AntDesign } from "@expo/vector-icons";
-import { useState } from "react";
-import {HOST_API} from '@env'
-import {NativeStackScreenProps} from "@react-navigation/native-stack";
+import { useState, useEffect } from "react";
+import { HOST_API } from '@env'
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { useGetUserById } from '../../hooks/useUserById';
+import useUpdateFavoriteCourts from "../../hooks/useUpdateFavoriteCourtsMutation";
+import useGetFavoritesCourtsById from "../../hooks/useGetFavoritesCourtsById";
+import storage from '../../utils/storage'
+import { calculateDistance } from "../../utils/calculateDistance";
 
-export default function FavoriteCourts({navigation, route}: NativeStackScreenProps<RootStackParamList, 'FavoriteCourts'>) {
-	const USER_ID = route.params.userID; //LEGÍVEL
-	const {data, error, loading} = useGetFavoriteById(USER_ID, USER_ID) 
-	const [color, setColor] = useState("red")
+export default function FavoriteCourts({ navigation, route }: NativeStackScreenProps<RootStackParamList, 'FavoriteCourts'>) {
+	const USER_ID = '9'; //LEGÍVEL
+	const { data, error, loading } = useGetFavoritesCourtsById(USER_ID)
+	const [color, setColor] = useState({});
+	const { data: userByIdData, error: userByIdError, loading: userByIdLoading } = useGetUserById(USER_ID)
+	const [userFavoriteCourts, setUserFavoriteCourts] = useState<Array<string>>([])
 
-	return(
+	useEffect(() => {
+		userByIdData?.usersPermissionsUser?.data?.attributes?.favorite_courts?.data?.forEach(item => {
+			setColor(prevColor => ({
+				...prevColor,
+				[item.id]: "red",
+			}));
+			setUserFavoriteCourts(prevCourts => [...prevCourts, item.id]);
+		})
+	}, [userByIdData])
+
+	const [userLocation, setUserLocation] = useState({
+		latitude: 0,
+		longitude: 0
+	})
+	useEffect(() => {
+		storage.load<{ latitude: string, longitude: string }>({
+			key: 'userGeolocation'
+		}).then(response => setUserLocation({ latitude: Number(response.latitude), longitude: Number(response.longitude) }))
+	}, [])
+
+	const [updateLikedCourts, { data: dataLike, error: errorLike, loading: loadingLike }] = useUpdateFavoriteCourts()
+	const [isLoading, setIsLoading] = useState(false)
+	const handleUpdateCourtLike = (courtId: string): void => {
+
+		const courtsData = [
+			...userFavoriteCourts
+		]
+
+		if (userFavoriteCourts?.includes(courtId)) {
+			setIsLoading(true)
+			const arrayWithoutDeletedItem = courtsData.filter(item => item !== courtId)
+
+			updateLikedCourts({
+				variables: {
+					userId: USER_ID,
+					favorite_courts: arrayWithoutDeletedItem
+				}
+			}).then(() => {
+				setColor(prevColor => ({
+					...prevColor,
+					[courtId]: "white",
+				}));
+			})
+				
+				.finally(() => {
+					setIsLoading(false)
+					setUserFavoriteCourts(arrayWithoutDeletedItem)
+				})
+		} else {
+			setIsLoading(true)
+			courtsData.push(courtId)
+
+			updateLikedCourts({
+				variables: {
+					userId: USER_ID,
+					favorite_courts: courtsData
+				}
+			}).then(() => {
+				setColor(prevColor => ({
+					...prevColor,
+					[courtId]: "red",
+				}));
+			})
+				
+				.finally(() => {
+					setIsLoading(false)
+					setUserFavoriteCourts(courtsData)
+				})
+		}
+	}
+
+
+	return (
 		<ScrollView className='flex-1 py-4 bg-zinc-600'>
 			{
 				!loading &&
-					data?.usersPermissionsUser.data.attributes.favorite_courts.data.map(courtType => (
-						<InfosCourt.Root category={courtType.attributes.court_types.data[0].attributes.name}>
-							{/*
-								TODO: INTEGRATE CORRECTLY WITH THE NEW ATTRIBUTE TYPE (ARRAY)
-							*/}
-							{
-								courtType.attributes.court_types.data[0].attributes.courts.data.map(courtInfo => (
-									<InfosCourt.Court imageUrl={{uri: HOST_API + courtInfo.attributes.photo.data[0]?.attributes.url, height: 90, width: 138}}>
-										<InfosCourt.Content 
-											lastScheduling={
-												courtInfo.attributes.court_availabilities.data[0]?.attributes.schedulings.data[0]?.attributes.date 
-													?
-												new Date(courtInfo?.attributes?.court_availabilities?.data[0]?.attributes?.schedulings?.data[courtInfo.attributes.court_availabilities.data[0].attributes.schedulings.data.length - 1]?.attributes?.date)
-													:
-												undefined
-											}>
-												<InfosCourt.ContentHeader courtName = {courtInfo.attributes.fantasy_name}>
-													<AntDesign name="heart" size={20} color={color} onPress={() => color == "white" ? setColor("red") : setColor("white")} />
-												</InfosCourt.ContentHeader>
-												<InfosCourt.ContentCourtType courtType={courtInfo.attributes.name} />
-												<InfosCourt.ContentDistance distance={'666Km'}/>
-										</InfosCourt.Content>
-									</InfosCourt.Court>
-								))
-							}
-						</InfosCourt.Root>
-					))
-			}
+				data?.courtTypes?.data?.map(courtType => (
+					<InfosCourt.Root category={courtType.attributes.name}>
+						{
+							courtType.attributes.courts.data.map(courtInfo => (
+								<InfosCourt.Court imageUrl={{ uri: HOST_API + courtInfo?.attributes?.photo?.data[0]?.attributes?.url, height: 90, width: 138 }}>
+									<InfosCourt.Content
+										lastScheduling={
 
+											courtInfo?.attributes?.court_availabilities?.data[0]?.attributes?.schedulings?.data[0]?.attributes?.date
+												?
+												new Date(courtInfo?.attributes?.court_availabilities?.data[0]?.attributes?.schedulings?.data[courtInfo?.attributes?.court_availabilities?.data[0]?.attributes?.schedulings?.data?.length - 1]?.attributes?.date)
+												:
+												undefined
+										}>
+										<InfosCourt.ContentHeader courtName={courtInfo?.attributes?.fantasy_name}>
+											<AntDesign
+												name="heart"
+												size={20}
+												color={color[courtInfo.id] || "red"}
+												onPress={() => handleUpdateCourtLike(courtInfo.id)}
+											/>
+										</InfosCourt.ContentHeader>
+										<InfosCourt.ContentCourtType courtType={courtInfo?.attributes?.name} />
+										{
+
+										}
+										<InfosCourt.ContentDistance distance={
+											(() => {
+												const distanceInMeters = calculateDistance(
+													userLocation.latitude,
+													userLocation.longitude,
+													Number(courtInfo.attributes.establishment.data.attributes.address.latitude),
+													Number(courtInfo.attributes.establishment.data.attributes.address.longitude)
+												);
+												return distanceInMeters >= 1000
+													? `${(distanceInMeters / 1000).toFixed(1)} Km`
+													: `${distanceInMeters.toFixed(0)} metros`;
+											})()
+										} />
+									</InfosCourt.Content>
+								</InfosCourt.Court>
+							))
+						}
+					</InfosCourt.Root>
+
+				))
+			}
 		</ScrollView>
 	)
 }
