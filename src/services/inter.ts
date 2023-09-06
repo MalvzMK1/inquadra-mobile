3fd27de9859d6a9de578e2d786b4bc2deecbd1c8
Matheus Alves <matheus.adeoliveira11@gmail.com>
2023-09-06 10:52:55 -0300

    Implementation with inter API in progress

--- src/services/inter.ts
+++ src/services/inter.ts
@@ -1,84 +1,75 @@
-import {Axios, AxiosResponse} from 'axios';
-import {CLIENT_ID, CLIENT_SECRET, INTER_API_CERTIFICATE, INTER_API_KEY} from '@env';
+import {ApolloClient, gql, InMemoryCache, MutationTuple, useMutation} from "@apollo/client";
+import {INTER_API} from '@env';
 
-const BASE_URL = 'https://pix.bcb.gov.br/api/v2'
-const SCOPE = 'pix.read pix.write webhook.read webhook.write cob.read cob.write cobv.read cobv.write payloadlocation.read payloadlocation.write\n';
-const TOKEN_URL = 'http://inter-oauth-server:8080/token';
-
-const axios = new Axios({
-	url: BASE_URL,
-	// httpsAgent: {
-	// 	cert: INTER_API_CERTIFICATE,
-	// 	key: INTER_API_KEY
-	// }
+export const client = new ApolloClient({
+	uri: INTER_API,
+	cache: new InMemoryCache()
 })
 
-interface IAuthTokenResponse {
-	access_token: string,
-	token_type: string,
-	expires_in: number,
-	scope: string,
-}
-
-export function generateAuthToken(): void {
-	try {
-		const params = new FormData();
-		//
-		params.append('grant_type', 'client_credentials');
-		params.append('client_id', CLIENT_ID);
-		params.append('client_secret', CLIENT_SECRET);
-		params.append('scope', SCOPE);
-
-		fetch(BASE_URL + '/token', {
-			method: 'POST',
-			body: params
-		})
-			.then(response => response.json())
-			.then(data => console.log(data.error.details))
-			.catch(error => console.warn(error))
-
-		// new Axios().get('https://inquadra-api-uat.qodeless.io/api/users')
-		// 	.then(response => response.data)
-		// 	.then(console.warn)
-
-		// axios.get('https://inquadra-api-uat.qodeless.io/api/users')
-		// 	.then(response => console.log({INTER_RESPONSE: response}))
-	} catch (e) {
-		console.error({error: e})
+interface ICreateChargeResponse {
+	txid: string;
+	pixCopiaECola: string;
+	status: string;
+	calendario: {
+		dataDeVencimento: string;
 	}
 }
 
-// const params = new URLSearchParams();
-// params.append('grant_type', 'client_credentials');
-// params.append('client_id', CLIENT_ID);
-// params.append('client_secret', CLIENT_SECRET);
-// params.append('scope', SCOPE);
-//
-// axios.post(httpInterOauthServer8080Token, params)
-// 	.then((response) => {
-// 		const authToken = response.data.access_token;
-// 		console.log('Token de Acesso:', authToken);
-// 		consultarPix(authToken); // Passa o authToken como argumento para a função
-// 	})
-// 	.catch((error) => {
-// 		console.error('Erro ao obter o Token de Acesso:', error);
-// 	});
+interface ICreateChargeVariables {
+	dueDate: string;
+	value: string;
+	debtorCpf: string;
+	debtorName: string;
+	debtorCep: string;
+	debtorStreet: string;
+	debtorCity: string;
+	debtorUf: string;
+	message: string;
+}
 
-// function consultarPix(authToken) {
-// 	const apiBaseUrl = 'https://cdpj.partners.bancointer.com.br/pix/v2/cob';
-// 	const endToPixId = '617c1e60-2b17-4ec2-990c-822750c0d899';
-//
-// 	const config = {
-// 		headers: {
-// 			'Authorization': `Bearer ${authToken}`,
-// 		}
-// 	};
-//
-// 	axios.get(`${apiBaseUrl}/pix/${endToPixId}`, config)
-// 		.then((response) => {
-// 			console.log('Dados do Pix:', response.data);
-// 		})
-// 		.catch((error) => {
-// 			console.error('Erro ao consultar o Pix:', error);
-// 		});
-// }
+const createChargeMutation = gql`
+    mutation DueCharge(
+        $dueDate: String!
+        $value: String!
+        $debtorCpf: String!
+        $debtorName: String!
+        $debtorCep: String!
+        $debtorStreet: String!
+        $debtorCity: String!
+        $debtorUf: String!
+        $message: String!
+    ) {
+        CreateCharge(
+            calendario: { dataDeVencimento: $dueDate, validadeAposVencimento: 30 }
+            valor: {
+                original: $value
+                desconto: {
+                    modalidade: "1"
+                    descontoDataFixa: [{ data: "2024-02-01", valorPerc: "0.00" }]
+                }
+                multa: { modalidade: "1", valorPerc: "0.00" }
+                juros: { modalidade: "1", valorPerc: "0.00" }
+            }
+            devedor: {
+                cpf: $debtorCpf
+                nome: $debtorName
+                cep: $debtorCep
+                logradouro: $debtorStreet
+                cidade: $debtorCity
+                uf: $debtorUf
+            }
+            solicitacaoPagador: $message
+        ) {
+            txid
+            pixCopiaECola
+		        status
+		        calendario {
+				        dataDeVencimento
+		        }
+        }
+    }
+`
+
+export function useCreateCharge(): MutationTuple<ICreateChargeResponse, ICreateChargeVariables> {
+	return useMutation<ICreateChargeResponse, ICreateChargeVariables>(createChargeMutation);
+}
