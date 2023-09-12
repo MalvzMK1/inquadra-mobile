871d1779b2544f6cd661d3bb40aad35f6481c0c0
vini00784 <vo70346@gmail.com>
2023-09-12 11:17:11 -0300

    renaming a file

--- src/hooks/useBlockSchedule.ts
+++ src/hooks/useBlockSchedule.ts
@@ -1,5 +1,5 @@
 import { useMutation, MutationTuple } from "@apollo/client";
-import { IBlockScheduleResponse, IBlockScheduleVariable, blockScheduleMutation } from "../graphql/mutations/blockSchedule";
+import { IBlockScheduleResponse, IBlockScheduleVariable, blockScheduleMutation } from "../graphql/mutations/blockScheduleByDate";
 
 export default function useBlockSchedule(): MutationTuple<IBlockScheduleResponse, IBlockScheduleVariable> {
     return useMutation<IBlockScheduleResponse, IBlockScheduleVariable>(blockScheduleMutation)
