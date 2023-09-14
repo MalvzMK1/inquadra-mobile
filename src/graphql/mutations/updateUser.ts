7fe0ca3bc8568f075c1e858b238e8ad4d4b824f9
Igor Medeiros <igor-dionisio@hotmail.com>
2023-09-14 17:56:55 -0300

    Fix: integration update user and update values profile page

--- src/graphql/mutations/updateUser.ts
+++ src/graphql/mutations/updateUser.ts
@@ -8,11 +8,11 @@ export interface IUpdateUserResponse {
                 email: User['email']
                 phoneNumber: User['phoneNumber']
                 cpf: User['cpf']
-        photo: {
-          data: {
-            id: Photo['id']
-          }
-        }
+                photo: {
+                    data: {
+                        id: Photo['id']
+                    }
+                }
                 role: {
                     data: {
                         id: Role['id']
@@ -30,7 +30,6 @@ export interface IUpdateUserVariables {
     email: string
     phone_number: string
     cpf: string
-    photo: string
     cvv?: number
     dueDate?: string
     country?: string
@@ -77,53 +76,4 @@ mutation updateUser(
       }
     }
   }  
-mutation updateUser(
-    $username: String!
-    $email: String!
-    $phone_number: String!
-    $cpf: String!
-    $user_id: ID!
-    $cvv: Int!
-    $dueDate: Date!
-    $country: ID!
-  ) {
-    updateUsersPermissionsUser(
-      id: $user_id
-      data: {
-        username: $username
-        email: $email
-        phoneNumber: $phone_number
-        cpf: $cpf
-      photo: $photo
-        paymentCardInformations: {
-          cvv: $cvv
-          dueDate: $dueDate
-          country: $country
-        }
-      }
-    ) {
-      data {
-        id
-        attributes {
-          username
-          email
-          phoneNumber
-          cpf
-        photo {
-          data {
-            id
-          }
-        }
-          role {
-            data {
-              attributes {
-                name
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-  
 `
