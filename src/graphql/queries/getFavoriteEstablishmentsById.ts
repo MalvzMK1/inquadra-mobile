d1ba1db3f29f42202a5fdf1fa12c258ce135fe95
vini00784 <vo70346@gmail.com>
2023-09-18 07:42:26 -0300

    hook to get favorite establishments by user id

---
+++ src/graphql/queries/getFavoriteEstablishmentsById.ts
@@ -0,0 +1,53 @@
+import { gql } from "@apollo/client";
+
+export interface IFavoriteEstablishmentsResponse {
+    establishments: {
+        data: Array<{
+            attributes: {
+                address: {
+                    latitude: Address['latitude']
+                    longitude: Address['longitude']
+                }
+                corporateName: Establishment['corporateName']
+                photos: {
+                    data: Array<{
+                        attributes: {
+                            url: Photo['url']
+                        }
+                    }>
+                }
+            }
+        }>
+    }
+}
+
+export const getFavoriteEstablishmentsQuery = gql`
+    query getFavoriteEstablishments($id: ID) {
+  establishments(
+    filters: {
+      favorited_users:{
+        id: {
+          eq: $id
+        }
+      }
+    }
+  ) {
+    data {
+      attributes {
+        address {
+          latitude
+          longitude
+        }
+        corporateName
+        photos {
+          data {
+            attributes {
+              url
+            }
+          }
+        }
+      }
+    }
+  }
+}
+`
