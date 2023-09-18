d8155b46e36522ed36aee13c8b1d4a66dd3303ef
vini00784 <vo70346@gmail.com>
2023-09-18 10:59:37 -0300

    getting the favorite establishments without images

--- src/screens/FavoriteEstablishments/index.tsx
+++ src/screens/FavoriteEstablishments/index.tsx
@@ -1,5 +1,5 @@
-import { ScrollView } from "react-native";
-import { InfosCourt } from "../../components/InfosCourt";
+import { ScrollView, Text } from "react-native";
+import { InfosEstablishment } from "../../components/InfosEstablishment";
 import { AntDesign } from "@expo/vector-icons";
 import { useState, useEffect } from "react";
 import { HOST_API } from '@env'
@@ -9,6 +9,8 @@ import useUpdateFavoriteCourts from "../../hooks/useUpdateFavoriteCourtsMutation
 import useGetFavoritesCourtsById from "../../hooks/useGetFavoritesCourtsById";
 import storage from '../../utils/storage'
 import { calculateDistance } from "../../utils/calculateDistance";
+import useGetFavoriteEstablishmentsByUserId from "../../hooks/useGetFavoriteEstablishmentsById";
+import useUpdateFavoriteEstablishment from "../../hooks/useUpdateFavoriteEstablishment";
 interface Colors {
     [key: string]: string;
 }
@@ -20,18 +22,19 @@ export default function FavoriteEstablishments({ navigation, route }: NativeStac
         }).then(data => setUserId(data.userId))
     }, [])
     const USER_ID = userId
-    const { data, error, loading } = useGetFavoritesCourtsById(USER_ID ?? "")
+    // const { data, error, loading } = useGetFavoritesCourtsById(USER_ID ?? "")
+    const { data, error, loading } = useGetFavoriteEstablishmentsByUserId(USER_ID ?? "")
     const [colors, setColors] = useState<Colors>({});
     const { data: userByIdData, error: userByIdError, loading: userByIdLoading } = useGetUserById(USER_ID ?? "")
-    const [userFavoriteCourts, setUserFavoriteCourts] = useState<Array<string>>([])
+    const [userFavoriteEstablishments, setUserFavoriteEstablishments] = useState<Array<string>>([])
 
     useEffect(() => {
-        userByIdData?.usersPermissionsUser?.data?.attributes?.favorite_courts?.data?.forEach(item => {
+        userByIdData?.usersPermissionsUser?.data?.attributes?.favorite_establishments?.data?.forEach(item => {
             setColors(prevColors => ({
                 ...prevColors,
                 [item.id]: "red",
             }));
-            setUserFavoriteCourts(prevCourts => [...prevCourts, item.id]);
+            setUserFavoriteEstablishments(prevEstablishments => [...prevEstablishments, item.id]);
         });
     }, [userByIdData]);
 
@@ -47,53 +50,53 @@ export default function FavoriteEstablishments({ navigation, route }: NativeStac
         }).then(response => setUserLocation({ latitude: Number(response.latitude), longitude: Number(response.longitude) }))
     }, [])
 
-    const [updateLikedCourts, { data: dataLike, error: errorLike, loading: loadingLike }] = useUpdateFavoriteCourts()
+    const [updateLikedEstablishments, { data: dataLike, error: errorLike, loading: loadingLike }] = useUpdateFavoriteEstablishment()
     const [isLoading, setIsLoading] = useState(false)
-    const handleUpdateCourtLike = (courtId: string): void => {
+    const handleUpdateEstablishmentLike = (establishmentId: string): void => {
 
-        const courtsData = [
-            ...userFavoriteCourts
+        const establishmentsData = [
+            ...userFavoriteEstablishments
         ]
 
-        if (userFavoriteCourts?.includes(courtId)) {
+        if (userFavoriteEstablishments?.includes(establishmentId)) {
             setIsLoading(true)
-            const arrayWithoutDeletedItem = courtsData.filter(item => item !== courtId)
+            const arrayWithoutDeletedItem = establishmentsData.filter(item => item !== establishmentId)
 
-            updateLikedCourts({
+            updateLikedEstablishments({
                 variables: {
-                    userId: USER_ID ?? "",
-                    favorite_courts: arrayWithoutDeletedItem
+                    user_id: USER_ID ?? "",
+                    favorite_establishments: arrayWithoutDeletedItem
                 }
             }).then(() => {
                 setColors(prevColor => ({
                     ...prevColor,
-                    [courtId]: "white",
+                    [establishmentId]: "white",
                 }));
             })
 
                 .finally(() => {
                     setIsLoading(false)
-                    setUserFavoriteCourts(arrayWithoutDeletedItem)
+                    setUserFavoriteEstablishments(arrayWithoutDeletedItem)
                 })
         } else {
             setIsLoading(true)
-            courtsData.push(courtId)
+            establishmentsData.push(establishmentId)
 
-            updateLikedCourts({
+            updateLikedEstablishments({
                 variables: {
-                    userId: USER_ID ?? "",
-                    favorite_courts: courtsData
+                    user_id: USER_ID ?? "",
+                    favorite_establishments: establishmentsData
                 }
             }).then(() => {
                 setColors(prevColor => ({
                     ...prevColor,
-                    [courtId]: "red",
+                    [establishmentId]: "red",
                 }));
             })
 
                 .finally(() => {
                     setIsLoading(false)
-                    setUserFavoriteCourts(courtsData)
+                    setUserFavoriteEstablishments(establishmentsData)
                 })
         }
     }
@@ -102,46 +105,82 @@ export default function FavoriteEstablishments({ navigation, route }: NativeStac
     return (
         <ScrollView className='flex-1 py-4 bg-zinc-600'>
             {
-                !loading &&
-                data?.courtTypes?.data?.map(courtType => (
-                    <InfosCourt.Root category={courtType.attributes.name} key={courtType.attributes.name}>
-                        {courtType.attributes.courts.data.map(courtInfo => {
-                            const schedulings = courtInfo?.attributes?.court_availabilities?.data[0]?.attributes?.schedulings?.data;
-                            const lastScheduling = schedulings && schedulings.length > 0
-                                ? new Date(schedulings[schedulings.length - 1].attributes.date)
-                                : undefined;
+                // !loading &&
+                // data?.courtTypes?.data?.map(courtType => (
+                //     <InfosEstablishment.Root category={courtType.attributes.name} key={courtType.attributes.name}>
+                //         {courtType.attributes.courts.data.map(courtInfo => {
+                //             const schedulings = courtInfo?.attributes?.court_availabilities?.data[0]?.attributes?.schedulings?.data;
+                //             const lastScheduling = schedulings && schedulings.length > 0
+                //                 ? new Date(schedulings[schedulings.length - 1].attributes.date)
+                //                 : undefined;
 
-                            return (
-                                <InfosCourt.Court key={courtInfo.id} imageUrl={{ uri: HOST_API + courtInfo?.attributes?.photo?.data[0]?.attributes?.url, height: 90, width: 138 }}>
-                                    <InfosCourt.Content lastScheduling={lastScheduling}>
-                                        <InfosCourt.ContentHeader courtName={courtInfo?.attributes?.fantasy_name}>
-                                            <AntDesign
-                                                name="heart"
-                                                size={20}
-                                                color={colors[courtInfo.id] || "red"}
-                                                onPress={() => handleUpdateCourtLike(courtInfo.id)}
-                                            />
-                                        </InfosCourt.ContentHeader>
-                                        <InfosCourt.ContentCourtType courtType={courtInfo?.attributes?.name} />
+                //             return (
+                //                 <InfosEstablishment.Court key={courtInfo.id} imageUrl={{ uri: HOST_API + courtInfo?.attributes?.photo?.data[0]?.attributes?.url, height: 90, width: 138 }}>
+                //                     <InfosEstablishment.Content lastScheduling={lastScheduling}>
+                //                         <InfosEstablishment.ContentHeader courtName={courtInfo?.attributes?.fantasy_name}>
+                //                             <AntDesign
+                //                                 name="heart"
+                //                                 size={20}
+                //                                 color={colors[courtInfo.id] || "red"}
+                //                                 onPress={() => handleUpdateEstablishmentLike(courtInfo.id)}
+                //                             />
+                //                         </InfosEstablishment.ContentHeader>
+                //                         <InfosEstablishment.ContentCourtType courtType={courtInfo?.attributes?.name} />
 
-                                        <InfosCourt.ContentDistance distance={
-                                            (() => {
-                                                const distanceInMeters = calculateDistance(
-                                                    userLocation.latitude,
-                                                    userLocation.longitude,
-                                                    Number(courtInfo.attributes.establishment.data.attributes.address.latitude),
-                                                    Number(courtInfo.attributes.establishment.data.attributes.address.longitude)
-                                                );
-                                                return distanceInMeters >= 1000
-                                                    ? `${(distanceInMeters / 1000).toFixed(1)} Km`
-                                                    : `${distanceInMeters.toFixed(0)} metros`;
-                                            })()
-                                        } />
-                                    </InfosCourt.Content>
-                                </InfosCourt.Court>
-                            );
-                        })}
-                    </InfosCourt.Root>
+                //                         <InfosEstablishment.ContentDistance distance={
+                //                             (() => {
+                //                                 const distanceInMeters = calculateDistance(
+                //                                     userLocation.latitude,
+                //                                     userLocation.longitude,
+                //                                     Number(courtInfo.attributes.establishment.data.attributes.address.latitude),
+                //                                     Number(courtInfo.attributes.establishment.data.attributes.address.longitude)
+                //                                 );
+                //                                 return distanceInMeters >= 1000
+                //                                     ? `${(distanceInMeters / 1000).toFixed(1)} Km`
+                //                                     : `${distanceInMeters.toFixed(0)} metros`;
+                //                             })()
+                //                         } />
+                //                     </InfosEstablishment.Content>
+                //                 </InfosEstablishment.Court>
+                //             );
+                //         })}
+                //     </InfosEstablishment.Root>
+                // ))
+
+
+            }
+
+            {
+                !loading && data?.establishments.data.map(item => (
+                    <InfosEstablishment.Root category="">
+                        <InfosEstablishment.Establishment imageUrl={{uri: item.attributes.photos.data[0].attributes.url}}>
+                            <InfosEstablishment.Content>
+                                <InfosEstablishment.ContentHeader establishmentName={item.attributes.corporateName}>
+                                    <AntDesign
+                                        name="heart"
+                                        size={20}
+                                        color={colors[item.id] || "red"}
+                                        onPress={() => handleUpdateEstablishmentLike(item.id)}
+                                    />
+                                </InfosEstablishment.ContentHeader>
+                                <Text>TORA GRANDE E GROSSA</Text>
+
+                                <InfosEstablishment.ContentDistance distance={
+                                    (() => {
+                                        const distanceInMeters = calculateDistance(
+                                            userLocation.latitude,
+                                            userLocation.longitude,
+                                            Number(item.attributes.address.latitude),
+                                            Number(item.attributes.address.longitude)
+                                        );
+                                        return distanceInMeters >= 1000
+                                            ? `${(distanceInMeters / 1000).toFixed(1)} Km`
+                                            : `${distanceInMeters.toFixed(0)} metros`;
+                                    })()
+                                } />
+                            </InfosEstablishment.Content>
+                        </InfosEstablishment.Establishment>
+                    </InfosEstablishment.Root>
                 ))
             }
         </ScrollView>
