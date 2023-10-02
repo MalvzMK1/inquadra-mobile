f02655d326f6b7f33141f8298a3957cdae2c380f
vini00784 <vo70346@gmail.com>
2023-10-02 07:07:48 -0300

    fixing error in the user profile image

--- src/screens/HomeEstablishment/index.tsx
+++ src/screens/HomeEstablishment/index.tsx
@@ -126,9 +126,11 @@ export default function HomeEstablishment({ navigation, route }: NativeStackScre
                     <Text className='text-lg font-bold text-white'>Olá, {firstName}!</Text>
                 </View>
                 <View className='h-max w-max flex justify-center items-center'>
-                    <TouchableOpacity className='h-12 W-12 '>
+                    <TouchableOpacity className='h-12 w-12' onPress={() => navigation.navigate("InfoProfileEstablishment", {
+                        userPhoto: dataSchedulings?.establishment?.data?.attributes?.logo?.data?.attributes?.url ?? ""
+                    })}>
                         <Image
-                            source={{ uri: HOST_API + dataSchedulings?.establishment?.data?.attributes?.logo?.data?.attributes?.url }}
+                            source={dataSchedulings?.establishment?.data?.attributes?.logo?.data?.attributes?.url ? { uri: HOST_API + dataSchedulings?.establishment?.data?.attributes?.logo?.data?.attributes?.url } : require("../../assets/default-user-image.png")}
                             style={{ width: 46, height: 46 }}
                             borderRadius={100}
                         />
