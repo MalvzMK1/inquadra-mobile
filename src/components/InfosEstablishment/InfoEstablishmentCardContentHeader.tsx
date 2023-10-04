4ee3978fc89cf18ba5e681260f5895755b5eb8f7
vini00784 <vo70346@gmail.com>
2023-10-04 10:31:00 -0300

    small changes in layout

--- src/components/InfosEstablishment/InfoEstablishmentCardContentHeader.tsx
+++ src/components/InfosEstablishment/InfoEstablishmentCardContentHeader.tsx
@@ -3,14 +3,12 @@ import {ReactNode} from "react";
 
 interface InfoEstablishmentCardContentHeaderProps {
 	establishmentName: string
-	children?: ReactNode
 }
 
-export function InfoEstablishmentCardContentHeader({establishmentName, children}: InfoEstablishmentCardContentHeaderProps) {
+export function InfoEstablishmentCardContentHeader({establishmentName}: InfoEstablishmentCardContentHeaderProps) {
 	return(
 		<View className='w-full flex flex-row justify-between items-center'>
 			<Text className='font-bold text-base text-orange-600'>{establishmentName}</Text>
-			{children}
 		</View>
 	)
 }
