fa693cfd2cbf21b07d5b109b745aa15270b2e691
Igor Medeiros <igor-dionisio@hotmail.com>
2023-11-29 17:29:45 -0300

    profile screen typos

--- src/screens/ProfileSettings/index.tsx
+++ src/screens/ProfileSettings/index.tsx
@@ -19,6 +19,7 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
+import {RNFetchBlob} from 'react-native-fetch-blob';
 import { SelectList } from "react-native-dropdown-select-list";
 import { showMessage } from "react-native-flash-message";
 import MaskInput, { Masks } from "react-native-mask-input";
@@ -39,7 +40,7 @@ import { getUsersCountryId } from "../../utils/getUsersCountryId";
 import { ALERT_TYPE, AlertNotificationRoot, Dialog } from "react-native-alert-notification";
 import storage from "../../utils/storage";
 import useUpdateUserPassword from "../../hooks/useUpdateUserPassword";
-import {ApolloError} from "@apollo/client";
+import { ApolloError } from "@apollo/client";
 
 interface IFormData {
   photo: string;
@@ -126,6 +127,8 @@ export default function ProfileSettings({
   const [showDeleteConfirmation, setShowDeleteConfirmation] = useState(false);
   const [showExitConfirmation, setShowExitConfirmation] = useState(false);
   const [countryId, setCountryId] = useState<string | number>();
+  const [photoData, setPhotoData] = useState<Photo>();
+
   const [countries, setCountries] = useState<Array<Country>>();
   const [countriesArray, setCountriesArray] = useState<
     Array<{ key: string; value: string }>
@@ -320,44 +323,49 @@ export default function ProfileSettings({
           setProfilePicture(result.assets[0].uri);
           setImageEdited(true);
           console.log("ID da imagem enviada:", uploadedImageID);
+        }).catch(error => {
+          console.error("Erro :", error);
         });
       }
     } catch (error) {
-      console.log("Erro ao carregar a imagem: ", error);
+      console.error("Erro ao carregar a imagem: ", error);
     }
   };
 
   const uploadImage = async (selectedImageUri: string) => {
     setIsLoading(true);
     const apiUrl = HOST_API;
-
-    const formData = new FormData();
-    formData.append("files", {
-      uri: selectedImageUri,
-      name: "profile.jpg",
-      type: "image/jpeg",
-    });
-
-    try {
-      const response = await axios.post(`${apiUrl}/api/upload`, formData, {
-        headers: {
-          "Content-Type": "multipart/form-data",
+  
+    try {  
+      const response = await RNFetchBlob.fetch(
+        'POST',
+        `${apiUrl}/api/upload`,
+        {
+          'Content-Type': 'multipart/form-data',
         },
-      });
-
+        [
+          {
+            name: 'files',
+            filename: 'profile.jpg',
+            data: RNFetchBlob.wrap(selectedImageUri),
+          },
+        ],
+      );
+  
       const uploadedImageID = response.data[0].id;
-
-      console.log("Imagem enviada com sucesso!", response.data);
-
+  
+      console.log('Imagem enviada com sucesso!', response.data);
+  
       setIsLoading(false);
-
+  
       return uploadedImageID;
     } catch (error) {
-      console.error("Erro ao enviar imagem:", JSON.stringify(error, null, 2));
+      console.error('Erro ao enviar imagem:', error);
       setIsLoading(false);
-      return "Deu erro";
+      return 'Deu erro';
     }
   };
+  
 
   interface IUpdateUserValidatingPhotoProps {
     user_id: string;
@@ -383,12 +391,15 @@ export default function ProfileSettings({
       })
     } else {
       await updateUser({
-        user_id: data.user_id,
-        email: data.email,
-        cpf: data.cpf,
-        phone_number: data.phone_number,
-        username: data.username,
-      })
+      variables: {
+          user_id: data.user_id,
+          email: data.email,
+          cpf: data.cpf,
+          phone_number: data.phone_number,
+          username: data.username,
+          photo: data.photo ?? ''
+        },
+      });
     }
   }
 
@@ -400,7 +411,7 @@ export default function ProfileSettings({
           uploadedImageID = await uploadImage(profilePicture);
 
           setPhoto(profilePicture);
-          console.log({photo});
+          console.log({ photo });
         }
 
         await updateUserValidatingPhoto({
@@ -438,18 +449,28 @@ export default function ProfileSettings({
 
   async function loadInformations() {
     let newUserInfos = userInfos;
+
     if (
       !loading &&
       data &&
       data.usersPermissionsUser.data
     ) {
+      setPhotoData({
+        id: data.usersPermissionsUser.data.attributes.photo.data?.id ?? '',
+        name:  data.usersPermissionsUser.data.attributes.photo.data?.attributes.name ?? '',
+        alternativeText: 'Request tela de perfil',
+        ext: 'jpg',
+        mime: 'image/jpg',
+        url:  data.usersPermissionsUser.data.attributes.photo.data?.attributes.url ?? '',
+        size: 1024,
+      })
       newUserInfos = {
         id: data.usersPermissionsUser.data.id,
         username: data.usersPermissionsUser.data.attributes.username,
         cpf: data.usersPermissionsUser.data.attributes.cpf,
         email: data.usersPermissionsUser.data.attributes.email,
         phoneNumber: data.usersPermissionsUser.data.attributes.phoneNumber,
-        photo: data.usersPermissionsUser.data.attributes.photo.data?.id ?? '',
+        photo: photoData,
         paymentCardInfos: {
           dueDate: data.usersPermissionsUser.data.attributes
             .paymentCardInformations.dueDate
@@ -553,7 +574,7 @@ export default function ProfileSettings({
         .catch((reason) => {
           if (reason instanceof ApolloError) {
             if (reason.message === 'The provided current password is invalid')
-              alert ('A senha atual informada não é válida')
+              alert('A senha atual informada não é válida')
             alert(reason.message)
           }
           alert('Erro na alteração de senha\n' + JSON.stringify(reason, null, 2))
