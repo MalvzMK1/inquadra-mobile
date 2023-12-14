ef39442ee35755ecd1434061f09a8fd9bebda154
llanunes <larissanunesvaz23@gmail.com>
2023-12-14 19:12:29 -0300

    Tela estabelecimento perfil jogador - o menu da tela de estabelecimento está cortando um pouco

--- src/screens/FinancialEstablishment/Client/WithdrawalScreen.tsx
+++ src/screens/FinancialEstablishment/Client/WithdrawalScreen.tsx
@@ -7,12 +7,12 @@ import {
 } from "@react-navigation/native";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React, { useState } from "react";
-import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
+import { FlatList, Text, TouchableOpacity, View } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
 import BottomBlackMenuEstablishment from "../../../components/BottomBlackMenuEstablishment";
 import NoticeCard from "../../../components/NoticeCard";
 import { useGetUserHistoricPayment } from "../../../hooks/useGetHistoricPayment";
 import { useGetUserIDByEstablishment } from "../../../hooks/useUserByEstablishmentID";
-import { ScrollView } from "react-native-gesture-handler";
 
 interface Props
   extends NativeStackScreenProps<RootStackParamList, "WithdrawScreen"> {
@@ -31,74 +31,86 @@ export default function WithdrawScreen({
     }>
   >([]);
   const [valueCollected, setValueCollected] =
-    useState<Array<{ valuePayment: number; payday: string; activated: boolean }>>();
+    useState<
+      Array<{ valuePayment: number; payday: string; activated: boolean }>
+    >();
   const [isWithdrawalMade, setIsWithdrawalMade] = useState(false);
   const [selectedPixKey, setSelectedPixKey] = useState<string>("0");
-  const [errorPop, setErrorPop] = useState<string>()
-  const [havePixKey, setHavePixKey] = useState<boolean>()
-  const [infos, setInfos] = useState<{ id: string; key: string; }[]>([])
+  const [errorPop, setErrorPop] = useState<string>();
+  const [havePixKey, setHavePixKey] = useState<boolean>();
+  const [infos, setInfos] = useState<{ id: string; key: string }[]>([]);
   const navigation = useNavigation<NavigationProp<RootStackParamList>>();
 
   const [number, setNumber] = useState(0);
-  const avaibleToCashOut = route.params.valueDisponible
+  const avaibleToCashOut = route.params.valueDisponible;
 
   useFocusEffect(
     React.useCallback(() => {
       setWithdrawalInfo([]);
       setValueCollected([]);
       if (!error && !loading) {
-        const amountPaid: { valuePayment: number; payday: string; activated: boolean }[] = [];
+        const amountPaid: {
+          valuePayment: number;
+          payday: string;
+          activated: boolean;
+        }[] = [];
         if (
           data &&
           data?.establishment.data.attributes.courts.data.length > 0
         ) {
           data?.establishment.data.attributes.courts.data.forEach(court => {
             if (court.attributes.court_availabilities.data.length > 0) {
-              court.attributes.court_availabilities.data.forEach(availability => {
-                if (availability.attributes.schedulings.data.length > 0) {
-                  availability.attributes.schedulings.data.forEach(schedulings => {
-                    if (schedulings.attributes.user_payments.data.length > 0) {
-                      schedulings.attributes.user_payments.data.forEach(payment => {
-                        amountPaid.push({
-                          valuePayment: payment.attributes.value,
-                          payday: schedulings.attributes.date,
-                          activated: schedulings.attributes.activated
-                        })
-                      })
-                    } else {
-                      setErrorPop("Não foi encontrado nenhum pagamento")
-                    }
-                  })
-                } else {
-                  setErrorPop("Não foi encontrado nenhum agendamento")
-                }
-
-              })
+              court.attributes.court_availabilities.data.forEach(
+                availability => {
+                  if (availability.attributes.schedulings.data.length > 0) {
+                    availability.attributes.schedulings.data.forEach(
+                      schedulings => {
+                        if (
+                          schedulings.attributes.user_payments.data.length > 0
+                        ) {
+                          schedulings.attributes.user_payments.data.forEach(
+                            payment => {
+                              amountPaid.push({
+                                valuePayment: payment.attributes.value,
+                                payday: schedulings.attributes.date,
+                                activated: schedulings.attributes.activated,
+                              });
+                            },
+                          );
+                        } else {
+                          setErrorPop("Não foi encontrado nenhum pagamento");
+                        }
+                      },
+                    );
+                  } else {
+                    setErrorPop("Não foi encontrado nenhum agendamento");
+                  }
+                },
+              );
             } else {
-              setErrorPop("Não foi encontrado nenhuma disponibilidade")
+              setErrorPop("Não foi encontrado nenhuma disponibilidade");
             }
-          })
+          });
         } else {
-          setErrorPop("Não foi encontrado nenhuma quadra registrada")
+          setErrorPop("Não foi encontrado nenhuma quadra registrada");
         }
-        
+
         if (
           data &&
           data.establishment.data.attributes.pix_keys.data.length > 0
         ) {
-          const infosHold = data?.establishment.data.attributes.pix_keys.data.map(
-            item => {
+          const infosHold =
+            data?.establishment.data.attributes.pix_keys.data.map(item => {
               return {
                 id: item.id,
                 key: item.attributes.key,
               };
-            },
-          );
-          setInfos(infosHold)
-          setHavePixKey(true)
+            });
+          setInfos(infosHold);
+          setHavePixKey(true);
           setWithdrawalInfo(prevState => [...prevState, ...infosHold]);
         } else {
-          setHavePixKey(false)
+          setHavePixKey(false);
         }
         if (amountPaid) {
           setValueCollected(prevState => {
@@ -114,7 +126,7 @@ export default function WithdrawScreen({
 
   function isAvailableForWithdrawal() {
     const validateActivatedStatus = valueCollected?.filter(item => {
-      return item.activated === true
+      return item.activated === true;
     });
 
     if (validateActivatedStatus) {
@@ -122,8 +134,7 @@ export default function WithdrawScreen({
         (total, current) => total + current.valuePayment,
         0,
       );
-    }
-    else return 0;
+    } else return 0;
   }
 
   const fixedWithdrawalAmounts: Array<number> = [50, 100, 250, 500];
@@ -152,7 +163,7 @@ export default function WithdrawScreen({
         navigation.navigate("HistoryPayment", {
           establishmentId: route.params.establishmentId,
           logo: route.params.logo,
-          dateFilter: null
+          dateFilter: null,
         }),
       1000,
     );
@@ -164,89 +175,94 @@ export default function WithdrawScreen({
   } = useGetUserIDByEstablishment(route.params.establishmentId);
 
   return (
-    <View> 
-    <ScrollView>
-      <View className="flex-1 justify-center items-center ">
-        {isWithdrawalMade ? (
-          <NoticeCard text="Saque realizado com sucesso" />
-        ) : (
-          <></>
-        )}
-        <View
-          className={`flex-1 pb-8 items-center justify-center z-10 ${isWithdrawalMade ? "opacity-50" : ""
+    <View>
+      <ScrollView>
+        <View className="flex-1 justify-center items-center ">
+          {isWithdrawalMade ? (
+            <NoticeCard text="Saque realizado com sucesso" />
+          ) : (
+            <></>
+          )}
+          <View
+            className={`flex-1 pb-8 items-center justify-center z-10 ${
+              isWithdrawalMade ? "opacity-50" : ""
             }`}
-        >
-          <View>
-            <View
-              className="flex-1 "
-              pointerEvents={isWithdrawalMade ? "none" : "auto"}
-            >
-              <View className="p-4 flex flex-col">
-                <View className="p-5 flex flex-col justify-between">
-                  <Text className="text-xl font-bold">Valor a retirar</Text>
-                </View>
-                <View className="p-3 items-center flex-row justify-center gap-5">
-                  <TouchableOpacity
-                    className="bg-gray-300 w-1/12 rounded-md"
-                    onPress={handleDecrement}
-                  >
-                    <Text className="text-3xl text-center text-gray-500">-</Text>
-                  </TouchableOpacity>
-                  <View>
-                    <Text className="font-extrabold text-3xl">
-                      R$ {number.toFixed(2)}
-                    </Text>
+          >
+            <View>
+              <View
+                className="flex-1 "
+                pointerEvents={isWithdrawalMade ? "none" : "auto"}
+              >
+                <View className="p-4 flex flex-col">
+                  <View className="p-5 flex flex-col justify-between">
+                    <Text className="text-xl font-bold">Valor a retirar</Text>
                   </View>
-                  <TouchableOpacity
-                    className="bg-gray-300 w-1/12 rounded-md"
-                    onPress={handleIncrement}
-                  >
-                    <Text className="text-3xl text-center text-gray-500">+</Text>
-                  </TouchableOpacity>
-                </View>
-                <Slider
-                  style={{ width: "100%", height: 40 }}
-                  minimumValue={0}
-                  maximumValue={avaibleToCashOut}
-                  step={0.1}
-                  value={number}
-                  onValueChange={handleSliderChange}
-                  minimumTrackTintColor="#FF6112"
-                  maximumTrackTintColor="gray"
-                  thumbTintColor="#FF6112"
-                />
-                <FlatList
-                  horizontal
-                  contentContainerStyle={{ gap: 8 }}
-                  data={fixedWithdrawalAmounts}
-                  keyExtractor={value => value.toString()}
-                  renderItem={({ item: value }) => {
-                    return (
-                      <TouchableOpacity
-                        disabled={avaibleToCashOut < value}
-                        className={`p-4 flex-row rounded-lg ${avaibleToCashOut >= value
-                          ? "bg-gray-400"
-                          : "bg-gray-300"
+                  <View className="p-3 items-center flex-row justify-center gap-5">
+                    <TouchableOpacity
+                      className="bg-gray-300 w-1/12 rounded-md"
+                      onPress={handleDecrement}
+                    >
+                      <Text className="text-3xl text-center text-gray-500">
+                        -
+                      </Text>
+                    </TouchableOpacity>
+                    <View>
+                      <Text className="font-extrabold text-3xl">
+                        R$ {number.toFixed(2)}
+                      </Text>
+                    </View>
+                    <TouchableOpacity
+                      className="bg-gray-300 w-1/12 rounded-md"
+                      onPress={handleIncrement}
+                    >
+                      <Text className="text-3xl text-center text-gray-500">
+                        +
+                      </Text>
+                    </TouchableOpacity>
+                  </View>
+                  <Slider
+                    style={{ width: "100%", height: 40 }}
+                    minimumValue={0}
+                    maximumValue={avaibleToCashOut}
+                    step={0.1}
+                    value={number}
+                    onValueChange={handleSliderChange}
+                    minimumTrackTintColor="#FF6112"
+                    maximumTrackTintColor="gray"
+                    thumbTintColor="#FF6112"
+                  />
+                  <FlatList
+                    horizontal
+                    contentContainerStyle={{ gap: 8 }}
+                    data={fixedWithdrawalAmounts}
+                    keyExtractor={value => value.toString()}
+                    renderItem={({ item: value }) => {
+                      return (
+                        <TouchableOpacity
+                          disabled={avaibleToCashOut < value}
+                          className={`p-4 flex-row rounded-lg ${
+                            avaibleToCashOut >= value
+                              ? "bg-gray-400"
+                              : "bg-gray-300"
                           }`}
-                        onPress={() => setNumber(value)}
-                      >
-                        <Text
-                          className={`${avaibleToCashOut >= value
-                            ? "text-white"
-                            : "text-gray-400"
-                            }`}
+                          onPress={() => setNumber(value)}
                         >
-                          R$ {value.toFixed(2)}
-                        </Text>
-                      </TouchableOpacity>
-                    );
-                  }}
-                />
+                          <Text
+                            className={`${
+                              avaibleToCashOut >= value
+                                ? "text-white"
+                                : "text-gray-400"
+                            }`}
+                          >
+                            R$ {value.toFixed(2)}
+                          </Text>
+                        </TouchableOpacity>
+                      );
+                    }}
+                  />
 
-                <View className="p-5 flex flex-col justify-between">
-                  {
-                    havePixKey
-                      ?
+                  <View className="p-5 flex flex-col justify-between">
+                    {havePixKey ? (
                       <>
                         <Text className="text-xl font-bold">
                           Selecione uma chave Pix
@@ -257,10 +273,11 @@ export default function WithdrawScreen({
                           renderItem={({ item: card }) => {
                             return (
                               <TouchableOpacity
-                                className={`p-5 flex-row rounded-lg mt-5 ${card.id == selectedPixKey
-                                  ? "bg-slate-500"
-                                  : "bg-gray-300"
-                                  }`}
+                                className={`p-5 flex-row rounded-lg mt-5 ${
+                                  card.id == selectedPixKey
+                                    ? "bg-slate-500"
+                                    : "bg-gray-300"
+                                }`}
                                 onPress={() => {
                                   if (card.id !== selectedPixKey)
                                     setSelectedPixKey(card.id);
@@ -276,58 +293,58 @@ export default function WithdrawScreen({
                           }}
                         />
                       </>
-                      : <Text className="text-sm text-gray-400 p-5">
+                    ) : (
+                      <Text className="text-sm text-gray-400 p-5">
                         Não foi encontrada nenhuma chave pix
                       </Text>
-                  }
-
+                    )}
+                  </View>
+                  <Text className="text-sm text-gray-400 p-5">
+                    Análise de pagamento em até 48 horas*
+                  </Text>
                 </View>
-                <Text className="text-sm text-gray-400 p-5">
-                  Análise de pagamento em até 48 horas*
-                </Text>
               </View>
-            </View>
-            <View className="items-center justify-center flex flex-row gap-5 pb-16">
-              <TouchableOpacity
-                className=" w-40 h-14  rounded-md bg-gray-300 flex items-center justify-center"
-                onPress={() => navigation.goBack()}
-              >
-                <Text className="font-bold text-gray-400">Cancelar</Text>
-              </TouchableOpacity>
-              <TouchableOpacity
-                className={`w-40 h-14 rounded-md flex items-center justify-center ${selectedPixKey === "0" || number === 0
-                  ? "bg-[#ffa363]"
-                  : "bg-[#FF6112]"
+              <View className="items-center justify-center flex flex-row gap-5 pb-16">
+                <TouchableOpacity
+                  className=" w-40 h-14  rounded-md bg-gray-300 flex items-center justify-center"
+                  onPress={() => navigation.goBack()}
+                >
+                  <Text className="font-bold text-gray-400">Cancelar</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  className={`w-40 h-14 rounded-md flex items-center justify-center ${
+                    selectedPixKey === "0" || number === 0
+                      ? "bg-[#ffa363]"
+                      : "bg-[#FF6112]"
                   }`}
-                disabled={selectedPixKey === "0" ? true : false}
-                onPress={withdrawalMade}
-              >
-                <Text className="text-gray-50 font-bold">Agendar Pix</Text>
-              </TouchableOpacity>
+                  disabled={selectedPixKey === "0" ? true : false}
+                  onPress={withdrawalMade}
+                >
+                  <Text className="text-gray-50 font-bold">Agendar Pix</Text>
+                </TouchableOpacity>
+              </View>
             </View>
           </View>
         </View>
-       
+      </ScrollView>
+      <View className={`absolute bottom-0 left-0 right-0`}>
+        <BottomBlackMenuEstablishment
+          screen="Any"
+          establishmentLogo={
+            dataUserEstablishment?.establishment?.data?.attributes?.logo?.data
+              ?.attributes?.url !== undefined ||
+            dataUserEstablishment?.establishment?.data?.attributes?.logo?.data
+              ?.attributes?.url !== null
+              ? HOST_API +
+                dataUserEstablishment?.establishment?.data?.attributes?.logo
+                  ?.data?.attributes?.url
+              : null
+          }
+          establishmentID={route.params.establishmentId}
+          key={1}
+          paddingTop={2}
+        />
       </View>
-    </ScrollView>
-    <View className={`absolute bottom-0 left-0 right-0`}>
-    <BottomBlackMenuEstablishment
-      screen="Any"
-      establishmentLogo={ 
-        dataUserEstablishment?.establishment?.data?.attributes?.logo?.data
-          ?.attributes?.url !== undefined ||
-          dataUserEstablishment?.establishment?.data?.attributes?.logo?.data
-            ?.attributes?.url !== null
-          ? HOST_API +
-          dataUserEstablishment?.establishment?.data?.attributes?.logo
-            ?.data?.attributes?.url
-          : null
-      }
-      establishmentID={route.params.establishmentId}
-      key={1}
-      paddingTop={2}
-    />
-  </View>
     </View>
   );
 }
