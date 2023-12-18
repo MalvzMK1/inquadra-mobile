7cc237d5887c421d456636e6045598bdecc32fb2
Matheus Alves <matheus.adeoliveira11@gmail.com>
2023-12-18 13:36:23 -0300

    fixed function to get range by date

--- src/utils/getDatesRange.ts
+++ src/utils/getDatesRange.ts
@@ -1,23 +1,23 @@
 export default function getDatesRange(initialDate: string, endDate: string) {
-	const dates: string[] = [];
+	try {
+		const dates: string[] = [];
 
-	let separatedInitialDate = initialDate.split("/");
-	const formatedInitialDate = `${separatedInitialDate[2]}-${separatedInitialDate[1]}-${separatedInitialDate[0]}`;
-	const initial = new Date(formatedInitialDate);
+		const initial = new Date(initialDate);
 
-	if (endDate == "") {
-		dates.push(new Date(initial).toISOString().split("T")[0]);
-		return dates;
-	} else {
-		let separatedEndDate = endDate.split("/");
-		const formatedEndDate = `${separatedEndDate[2]}-${separatedEndDate[1]}-${separatedEndDate[0]}`;
-		const end = new Date(formatedEndDate);
-
-		while (initial <= end) {
+		if (endDate == "") {
 			dates.push(new Date(initial).toISOString().split("T")[0]);
-			initial.setDate(initial.getDate() + 1);
-		}
+			return dates;
+		} else {
+			const end = new Date(endDate);
 
-		return dates;
+			while (initial <= end) {
+				dates.push(new Date(initial).toISOString().split("T")[0]);
+				initial.setDate(initial.getDate() + 1);
+			}
+
+			return dates;
+		}
+	} catch(error) {
+		throw new Error(String(error));
 	}
 }
